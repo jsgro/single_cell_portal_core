a9ae34c147fc5a96fd7aa8238c5101e9691f1c42
Devon Bush <dbush@broadinstitute.org>
2020-06-11 10:28:32 -0400

    fixing covid19 page bug

--- app/javascript/components/covid19/Covid19PageContent.js
+++ app/javascript/components/covid19/Covid19PageContent.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Router } from '@reach/router'
 
 import SearchPanel from 'components/search/controls/SearchPanel'
 import ResultsPanel from 'components/search/results/ResultsPanel'
-import StudySearchProvider from 'providers/StudySearchProvider'
+import StudySearchProvider, { StudySearchContext } from 'providers/StudySearchProvider'
 import SearchFacetProvider from 'providers/SearchFacetProvider'
 import UserProvider from 'providers/UserProvider'
 import ErrorBoundary from 'lib/ErrorBoundary'
+import StudyDetails from 'components/search/results/Study'
+
 
 /**
  * Wrapper component for search and result panels
@@ -14,7 +16,7 @@ import ErrorBoundary from 'lib/ErrorBoundary'
 export default function Covid19PageContent() {
   return (
     <Router>
-      <CovidRawPageContent default/>
+      <CovidPageContainer default/>
     </Router>
   )
 }
@@ -24,23 +26,30 @@ export default function Covid19PageContent() {
  * Note this needs to be used within a Reach <Router> element or
  * the search component's useLocation hooks will error
  */
-function CovidRawPageContent() {
+function CovidPageContainer() {
   return (
     <ErrorBoundary>
       <UserProvider>
         <SearchFacetProvider>
           <StudySearchProvider preset="covid19" >
-            <ErrorBoundary>
-              <SearchPanel showCommonButtons={false}
-                keywordPrompt="Search within COVID-19 studies"
-                searchOnLoad={true}/>
-            </ErrorBoundary>
-            <ErrorBoundary>
-              <ResultsPanel/>
-            </ErrorBoundary>
+            <CovidPageSearchContent/>
           </StudySearchProvider>
         </SearchFacetProvider>
       </UserProvider>
     </ErrorBoundary>
   )
+}
+
+function CovidPageSearchContent() {
+  const studySearchState = useContext(StudySearchContext)
+  return (<>
+    <ErrorBoundary>
+      <SearchPanel showCommonButtons={false}
+        keywordPrompt="Search within COVID-19 studies"
+        searchOnLoad={true}/>
+    </ErrorBoundary>
+    <ErrorBoundary>
+      <ResultsPanel studySearchState={studySearchState} studyComponent={StudyDetails}/>
+    </ErrorBoundary>
+  </>)
 }
