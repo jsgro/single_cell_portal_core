e1882e660efcef1e4da0c1380df63ac6c79a599c
bistline <bistline@broadinstitute.org>
2020-06-12 10:19:56 -0400

    remove --force for initial install

--- rails_startup.bash
+++ rails_startup.bash
@@ -13,7 +13,7 @@ then
     # see https://github.com/rails/webpacker/issues/1189#issuecomment-359360326
     export NODE_OPTIONS="--max-old-space-size=4096"
     sudo -E -u app -H bundle exec rake NODE_ENV=production RAILS_ENV=$PASSENGER_APP_ENV SECRET_KEY_BASE=$SECRET_KEY_BASE assets:clean
-    sudo -E -u app -H NODE_ENV=production RAILS_ENV=$PASSENGER_APP_ENV yarn install --force
+    sudo -E -u app -H NODE_ENV=production RAILS_ENV=$PASSENGER_APP_ENV yarn install
     if [ $? -ne 0 ]; then
         # since node-sass has OS-dependent bindings, calling upgrade will force those bindings to install and prevent
         # the call to rake assets:precompile from failing due to the 'vendor' directory not being there
