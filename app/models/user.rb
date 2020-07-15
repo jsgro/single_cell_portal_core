8686c5339e2fe4ae7d18ae67964639dc252cb557
bistline <bistline@broadinstitute.org>
2020-07-15 14:49:13 -0400

    removing commented code

--- app/models/user.rb
+++ app/models/user.rb
@@ -325,29 +325,6 @@ class User
     end
   end
 
-  # # merges the user flags with the defaults -- this should  always be used in place of feature_flags
-  # # for determining whether to enable a feature for a given user.
-  # def feature_flags_with_defaults
-  #   FeatureFlag.default_flag_hash.merge(feature_flags ? feature_flags : {})
-  # end
-  #
-  # # gets the feature flag value for a given user, and the default value if no user is given
-  # def self.feature_flag_for_user(user, flag_key)
-  #   if user.present?
-  #     user.feature_flags_with_defaults[flag_key]
-  #   else
-  #     FeatureFlag.find_by(name: flag_key)&.default_value
-  #   end
-  # end
-  #
-  # # returns feature_flags_with_defaults for the user, or the default flags if no user is given
-  # def self.feature_flags_for_user(user)
-  #   if user.nil?
-  #     return FeatureFlag.default_flag_hash
-  #   end
-  #   user.feature_flags_with_defaults
-  # end
-
   # helper method to migrate study ownership & shares from old email to new email
   def self.migrate_studies_and_shares(existing_email, new_email)
     existing_user = self.find_by(email: existing_email)
