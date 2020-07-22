7a984f67432ff247b5d823ede5d049d51fadf5db
Eric Weitz <eweitz@broadinstitute.org>
2020-07-22 15:51:03 -0400

    Fix anonId value for Mixpanel identify call

--- test/integration/lib/metrics_service_test.rb
+++ test/integration/lib/metrics_service_test.rb
@@ -54,4 +54,39 @@ class MetricsServiceTest < ActiveSupport::TestCase
 
     puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
+
+  test 'should post expected data to Mixpanel `identify` endpoint' do
+    puts "#{File.basename(__FILE__)}: #{self.method_name}"
+
+    # A high-fidelity test double
+    user = User.new(access_token: {
+      access_token: 'foo',
+      expires_at: DateTime.new(3000, 1, 1)
+    })
+
+    cookies = {
+      user_id: '168d8f62-f813-4e45-61d7-b81afe29642a' # Random UUIDv4 string
+    }
+    anon_id = cookies['user_id']
+
+    # As input into RestClient::Request.execute.
+    # These expected arguments are the main thing we are testing.
+    expected_args = {
+      url: "https://terra-bard-dev.appspot.com/api/identify",
+      headers: {Authorization: "Bearer ", "Content-Type": "application/json"},
+      payload: {anonId: anon_id}.to_json,
+      method: "POST"
+    }
+
+    # Mock network traffic to/from Bard, the DSP service proxying Mixpanel
+    mock = Minitest::Mock.new
+    mock.expect :call, mock, [expected_args] # Mock `execute` call (request)
+
+    RestClient::Request.stub :execute, mock do
+      response = MetricsService.merge_identities_in_mixpanel(user, cookies)
+      mock.verify
+    end
+
+    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
+  end
 end
