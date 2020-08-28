f24d23dfb42e276ad18f2c558055937310984545
Eric Weitz <eweitz@broadinstitute.org>
2020-08-28 15:48:23 -0400

    Add TODOs regarding this gotcha

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,9 @@ module.exports = {
      // Bard client exports native ES6.
      // Jest can't import that without this line, but all modern browsers can.
      // This prevents SCP JavaScript tests from all throwing false positives.
+     //
+     // TODO (SCP-2705): Transpile Bard client to ease reuse
+     // TODO (SCP-2688): Update Bard client docs to note dependents need this line
     'node_modules/(?!@databiosphere/bard-client)'
   ],
   setupFilesAfterEnv: ['./test/js/setup-tests.js'],
