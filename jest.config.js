526de20ed38031818419259857035fb74f70cf90
Eric Weitz <eweitz@broadinstitute.org>
2020-08-28 15:30:54 -0400

    Account for native ES6 nature of Bard client
    Prevents all JS tests from throwing false positives

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,21 @@
 module.exports = {
   verbose: true,
   transform: {
-    "^.+\\.jsx?$": "babel-jest"
+    '^.+\\.jsx?$': 'babel-jest'
   },
+  transformIgnorePatterns: [
+     // Bard client exports native ES6.
+     // Jest can't import that without this line, but all modern browsers can.
+     // This prevents SCP JavaScript tests from all throwing false positives.
+    'node_modules/(?!@databiosphere/bard-client)'
+  ],
   setupFilesAfterEnv: ['./test/js/setup-tests.js'],
   testPathIgnorePatterns: [
-    "config/webpack/test.js"
+    'config/webpack/test.js'
   ],
   moduleDirectories: [
     'app/javascript',
     'app/assets',
     'node_modules'
   ]
-};
+}
