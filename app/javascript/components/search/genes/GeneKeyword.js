819230b9abb5c33619f79d4321b6e4be2c9dcbe2
Devon Bush <dbush@broadinstitute.org>
2020-09-30 15:06:42 -0400

    adding comma support in gene keyword search

--- app/javascript/components/search/genes/GeneKeyword.js
+++ app/javascript/components/search/genes/GeneKeyword.js
@@ -48,12 +48,13 @@ export default function GeneKeyword({ placeholder, helpTextContent }) {
 
   /** Converts any current typed free text to a gene array entry */
   function syncGeneArrayToInputText() {
-    const inputTextTrimmed = inputText.trim()
+    const inputTextTrimmed = inputText.trim().replace(',', '')
     if (!inputTextTrimmed) {
       return geneArray
     }
     const newGeneArray = [...geneArray, { label: inputTextTrimmed, value: inputTextTrimmed }]
-    setInputText('')
+
+    setInputText(' ')
     setGeneArray(newGeneArray)
     return newGeneArray
   }
@@ -65,7 +66,9 @@ export default function GeneKeyword({ placeholder, helpTextContent }) {
     }
     switch (event.key) {
       case ' ':
+      case ',':
         syncGeneArrayToInputText()
+        setTimeout(() => { setInputText(' ') }, 0)
     }
   }
 
