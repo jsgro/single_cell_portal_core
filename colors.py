7ccee07481eec5c8fbe7dae21448885a817723d7
Denis Loginov <denis@broadinstitute.org>
2020-10-01 18:23:48 -0400

    Test severity ANSI colors

--- colors.py
+++ colors.py
@@ -8,4 +8,9 @@ for i in range(0, 16):
         sys.stdout.write(u"\u001b[38;5;" + code + "m " + code.ljust(4))
     print(u"\u001b[0m")
 
+print(u"\033[90m;1 Information")
+print(u"\033[34m;1 Low")
+print(u"\033[33m;1 Medium")
+print(u"\033[31m;1 High")
+
 sys.exit(1)
