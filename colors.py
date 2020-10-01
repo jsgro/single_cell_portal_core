aea70296d8b38da76e5eee0440d8fb7ee2896bc0
Denis Loginov <denis@broadinstitute.org>
2020-10-01 18:27:16 -0400

    Further color testing

--- colors.py
+++ colors.py
@@ -8,9 +8,13 @@ for i in range(0, 16):
         sys.stdout.write(u"\u001b[38;5;" + code + "m " + code.ljust(4))
     print(u"\u001b[0m")
 
-print(u"\033[90m;1 Information")
-print(u"\033[34m;1 Low")
-print(u"\033[33m;1 Medium")
-print(u"\033[31m;1 High")
+print(u"\033[90;1m Information")
+print(u"\033[34;1m Low")
+print(u"\033[33;1m Medium")
+print(u"\033[31;1m High")
+
+
+for color in (30, 31, 32, 33, 34, 35, 36, 37):
+    print(f"\u001b[{color};1m {color}")
 
 sys.exit(1)
