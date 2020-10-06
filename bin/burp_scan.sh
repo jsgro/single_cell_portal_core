0beece88fa4754cdf2365c16b00ce4814163f090
Denis Loginov <denis@broadinstitute.org>
2020-10-05 23:24:50 -0400

    [burp] Upload reports to GCS bucket

--- bin/burp_scan.sh
+++ bin/burp_scan.sh
@@ -6,6 +6,13 @@ set -eu
 # authenticated with container registry using burp_start.sh)
 IMAGE="$1"
 
+# GCS bucket to upload reports to
+BUCKET="$2"
+
+# Service name for reporting
+SERVICE="$3"
+
 # Scan collected traffic and report results (optional)
 docker run --rm -it --net host --entrypoint /automation/BroadBurpScanner.py "${IMAGE}" \
-  http://localhost --action scan
+  http://localhost --action scan \
+    --report-bucket "${BUCKET}" --report-service "${SERVICE}" --report-type HTML XML
