bc9f6eabab2d5dfef38d438c7a867bb2987664e9
bistline <bistline@broadinstitute.org>
2020-10-06 13:27:47 -0400

    fixing reference to user object

--- test/integration/download_agreement_test.rb
+++ test/integration/download_agreement_test.rb
@@ -24,7 +24,7 @@ class DownloadAgreementTest < ActionDispatch::IntegrationTest
 
     # test bulk download
     get download_bulk_files_path(accession: @study.accession, study_name: @study.url_safe_name,
-                                 download_object: 'all', totat: @user.create_totat)
+                                 download_object: 'all', totat: @test_user.create_totat)
     assert_response :success, "Did not get curl config for bulk download"
 
     # enable download agreement, assert 403
@@ -35,7 +35,7 @@ class DownloadAgreementTest < ActionDispatch::IntegrationTest
     get download_file_path(accession: @study.accession, study_name: @study.url_safe_name, filename: file.upload_file_name)
     assert_response :forbidden, "Did not correctly respond 403 when download agreement is in place: #{response.code}"
     get download_bulk_files_path(accession: @study.accession, study_name: @study.url_safe_name,
-                                 download_object: 'all', totat: @user.create_totat)
+                                 download_object: 'all', totat: @test_user.create_totat)
     assert_response :forbidden, "Did not correctly respond 403 for bulk download: #{response.code}"
     assert response.body.include?('Download agreement'), "Error response did not reference download agreement: #{response.body}"
 
@@ -48,7 +48,7 @@ class DownloadAgreementTest < ActionDispatch::IntegrationTest
     signed_url = response.headers['Location']
     assert signed_url.include?(file.upload_file_name), "Redirect url does not point at requested file"
     get download_bulk_files_path(accession: @study.accession, study_name: @study.url_safe_name,
-                                 download_object: 'all', totat: @user.create_totat)
+                                 download_object: 'all', totat: @test_user.create_totat)
     assert_response :success, "Did not get curl config for bulk download after accepting download agreement"
 
     # clean up
