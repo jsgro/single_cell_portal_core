575259fdf657b75f75a6df225f0062e0a8406162
Devon Bush <dbush@broadinstitute.org>
2020-10-26 09:11:51 -0400

    fixing token auth_code test

--- test/api/search_controller_test.rb
+++ test/api/search_controller_test.rb
@@ -252,11 +252,9 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     )
     assert_response :unauthorized
 
-    execute_http_request(:get, api_v1_search_bulk_download_path(
-        auth_code: ' ', accessions: study.accession, file_types: file_types)
-    )
-
-    assert_response :unauthorized
+    execute_http_request(:get, api_v1_search_bulk_download_path(accessions: study.accession, file_types: file_types))
+    # response should be successful with no auth_code, since bearer token is valid
+    assert_response :success
 
     puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
