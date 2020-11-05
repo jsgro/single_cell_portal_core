ba2b7e09a409ed661e77a5744db7185b034242c0
bistline <bistline@broadinstitute.org>
2020-11-05 10:54:35 -0500

    fixing helper, adding correct scope

--- test/bulk_download_helper.rb
+++ test/bulk_download_helper.rb
@@ -3,12 +3,10 @@
 def bulk_download_response(study_files)
   response = {}
   study_files.each do |study_file|
-    format_file_for_response(response, study_file)
-    if study_file.is_bundled?
-      study_file.bundled_files.each do |bundled_file|
-        format_file_for_response(response, bundled_file)
-      end
-    end
+    file_type = study_file.simplified_file_type
+    response[file_type] ||= {total_files: 0, total_bytes: 0}
+    response[file_type][:total_files] += 1
+    response[file_type][:total_bytes] += study_file.upload_file_size
   end
   response.with_indifferent_access
 end
@@ -16,13 +14,4 @@ end
 # take response from bulk_download_response or API response and return total file count
 def get_file_count_from_response(response)
   response.values.map {|entry| entry[:total_files]}.reduce(&:+)
-end
-
-private
-
-def format_file_for_response(response_object, file)
-  file_type = file.simplified_file_type
-  response_object[file_type] ||= {total_files: 0, total_bytes: 0}
-  response_object[file_type][:total_files] += 1
-  response_object[file_type][:total_bytes] += file.upload_file_size
 end
