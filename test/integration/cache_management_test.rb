d008e7357f862a163eb852a7c4ec73f114116301
Eric Weitz <eweitz@broadinstitute.org>
2020-11-16 15:51:46 -0500

    Remove obsolete tests; ports upcoming in ticketed work

--- test/integration/cache_management_test.rb
+++ test/integration/cache_management_test.rb
@@ -21,7 +21,6 @@ class CacheManagementTest < ActionDispatch::IntegrationTest
       puts "Testing with annotation: #{annotation}"
 
       # get various actions subject to caching
-      get render_cluster_path(accession: study.accession, study_name: study.url_safe_name, cluster: cluster.name, annotation: annotation), xhr: true
       get render_gene_expression_plots_path(accession: study.accession, study_name: study.url_safe_name, cluster: cluster.name, annotation: annotation, gene: gene, plot_type: 'violin'), xhr: true
       get render_gene_set_expression_plots_path(accession: study.accession, study_name: study.url_safe_name, cluster: cluster.name, annotation: annotation, search: {genes: genes.join(' ')}, plot_type: 'violin', 'boxpoints':'all'), xhr: true
       get render_gene_expression_plots_path(accession: study.accession, study_name: study.url_safe_name, cluster: cluster.name, annotation: annotation, gene: gene, plot_type: 'box'), xhr: true
@@ -30,7 +29,6 @@ class CacheManagementTest < ActionDispatch::IntegrationTest
       get annotation_query_path(accession: study.accession, study_name: study.url_safe_name, annotation: annotation, cluster: cluster.name), xhr: true
 
       # construct various cache keys for direct lookup (cannot lookup via regex)
-      cluster_cache_key = "views/localhost/single_cell/study/#{study.accession}/#{study.url_safe_name}/render_cluster_#{cluster.name.split.join('-')}_#{annotation}.js"
       v_expression_cache_key = "views/localhost/single_cell/study/#{study.accession}/#{study.url_safe_name}/render_gene_expression_plots/#{gene}_#{cluster.name.split.join('-')}_#{annotation}_violin.js"
       v_set_expression_cache_key = "views/localhost/single_cell/study/#{study.accession}/#{study.url_safe_name}/render_gene_set_expression_plots_#{cluster.name.split.join('-')}_#{annotation}_#{genes_hash}_violin_all.js"
       b_expression_cache_key = "views/localhost/single_cell/study/#{study.accession}/#{study.url_safe_name}/render_gene_expression_plots/#{gene}_#{cluster.name.split.join('-')}_#{annotation}_box.js"
@@ -38,7 +36,6 @@ class CacheManagementTest < ActionDispatch::IntegrationTest
       exp_query_cache_key = "views/localhost/single_cell/study/#{study.accession}/#{study.url_safe_name}/expression_query_#{cluster.name.split.join('-')}_#{annotation}__#{genes_hash}.js"
       annot_query_cache_key = "views/localhost/single_cell/study/#{study.accession}/#{study.url_safe_name}/annotation_query_#{cluster.name.split.join('-')}_#{annotation}.js"
 
-      assert Rails.cache.exist?(cluster_cache_key), "Did not find matching cluster cache entry at #{cluster_cache_key}"
       assert Rails.cache.exist?(v_expression_cache_key), "Did not find matching gene expression cache entry at #{v_expression_cache_key}"
       assert Rails.cache.exist?(v_set_expression_cache_key), "Did not find matching gene set expression cache entry at #{v_set_expression_cache_key}"
       assert Rails.cache.exist?(b_expression_cache_key), "Did not find matching gene expression cache entry at #{b_expression_cache_key}"
@@ -47,12 +44,9 @@ class CacheManagementTest < ActionDispatch::IntegrationTest
       assert Rails.cache.exist?(annot_query_cache_key), "Did not find matching annotation query cache entry at #{annot_query_cache_key}"
 
       # load removal keys via associated study files
-      cluster_file_cache_key = cluster_file.cache_removal_key
       expression_file_cache_key = expression_file.cache_removal_key
 
       # clear caches individually and assert removals
-      CacheRemovalJob.new(cluster_file_cache_key).perform
-      assert_not Rails.cache.exist?(cluster_cache_key), "Did not delete matching cluster cache entry at #{cluster_cache_key}"
       CacheRemovalJob.new(expression_file_cache_key).perform
       assert_not Rails.cache.exist?(v_expression_cache_key), "Did not delete matching gene expression cache entry at #{v_expression_cache_key}"
       assert_not Rails.cache.exist?(v_set_expression_cache_key), "Did not delete matching gene set expression cache entry at #{v_set_expression_cache_key}"
