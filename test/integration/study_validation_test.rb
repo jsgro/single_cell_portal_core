504b7753ee2cc1030e76058c152270167f0383c1
bistline <bistline@broadinstitute.org>
2020-11-23 16:53:04 -0500

    attempting to fix I/O flush issue

--- test/integration/study_validation_test.rb
+++ test/integration/study_validation_test.rb
@@ -287,10 +287,14 @@ class StudyValidationTest < ActionDispatch::IntegrationTest
       entry['study_accession'] = study.accession
       entry['file_id'] = metadata_file.id.to_s
     end
-    tmp_file = File.new(Rails.root.join('tmp_bq_seeds.json'), 'w+')
+    puts "Data read, writing to newline-delimited JSON"
+    tmp_filename = SecureRandom.uuid + '.json'
+    tmp_file = File.new(Rails.root.join(tmp_filename), 'w+')
     tmp_file.write bq_data.map(&:to_json).join("\n")
+    puts "Data assembled, writing to BigQuery"
     table = ApplicationController.big_query_client.dataset(CellMetadatum::BIGQUERY_DATASET).table(CellMetadatum::BIGQUERY_TABLE)
     job = table.load(tmp_file, write: 'append', format: :json)
+    puts "Write complete, closing/removing files"
     bq_seeds.close
     tmp_file.close
     puts "BigQuery seeding completed: #{job}"
