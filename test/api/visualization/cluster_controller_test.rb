94528da838af82c58d6e761bac35401b96232257
bistline <bistline@broadinstitute.org>
2020-12-03 12:33:14 -0500

    adding test for clusters w/ slashes, improved cleanup in cluster_controller_test.rb

--- test/api/visualization/cluster_controller_test.rb
+++ test/api/visualization/cluster_controller_test.rb
@@ -39,6 +39,10 @@ class ClusterControllerTest < ActionDispatch::IntegrationTest
   end
 
   teardown do
+    StudyFile.where(study_id: @basic_study.id).destroy_all
+    DataArray.where(study_id: @basic_study.id).destroy_all
+    ClusterGroup.where(study_id: @basic_study.id).destroy_all
+    CellMetadatum.where(study_id: @basic_study.id).destroy_all
     @basic_study.destroy
     @empty_study.destroy
     @user.destroy
@@ -63,6 +67,35 @@ class ClusterControllerTest < ActionDispatch::IntegrationTest
     execute_http_request(:get, api_v1_study_clusters_path(@empty_study))
     assert_equal 404, response.status
     assert_equal({"error"=>"No default cluster exists"}, json)
+
+    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
+  end
+
+  test 'should load clusters with slashes in name' do
+    puts "#{File.basename(__FILE__)}: #{self.method_name}"
+
+    @cluster_with_slash = FactoryBot.create(:cluster_file,
+                                            name: 'data/cluster_with_slash.txt',
+                                            study: @basic_study,
+                                            cell_input: {
+                                              x: [1, 2 , 3, 4],
+                                              y: [5, 6, 7, 8],
+                                              cells: %w(A B C D)
+                                            },
+                                            annotation_input: [{name: 'category', type: 'group', values: ['bar', 'bar', 'baz', 'bar']}])
+
+    # slash must be URL encoded with %2F, and URL constructed manually as the path helper cannot resolve cluster_name param
+    # with a slash due to the route constraint of {:cluster_name=>/[^\/]+/}; using route helper api_v1_study_cluster_path()
+    # with the params of cluster_name: 'data/cluster_with_slash.txt' or cluster_name: 'data%2Fcluster_with_slash' does not work
+    basepath = "/single_cell/api/v1/studies/#{@basic_study.accession}/clusters/data%2Fcluster_with_slash.txt"
+    query_string = '?annotation_name=category&annotation_type=group&annotation_scope=cluster'
+    url = basepath + query_string
+    execute_http_request(:get, url)
+    assert_response :success
+    assert_equal 4, json['numPoints']
+    expected_annotations = ["bar (3 points)", "baz (1 points)"]
+    loaded_annotations = json['data'].map {|d| d['name'] }
+    assert_equal expected_annotations, loaded_annotations
 
     puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
