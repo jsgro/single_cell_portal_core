429e684b4d73ef894820f408551ec26833d67892
bistline <bistline@broadinstitute.org>
2021-01-05 12:08:34 -0500

    re-enabling tests, changing Travis machine OS to bionic

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -99,14 +99,14 @@ then
   fi
 else
   echo "Running all unit & integration tests..."
-  # yarn ui-test
+  yarn ui-test
   code=$? # immediately capture exit code to prevent this from getting clobbered
   if [[ $code -ne 0 ]]; then
     RETURN_CODE=$code
     first_test_to_fail=${first_test_to_fail-"yarn ui-test"}
     ((FAILED_COUNT++))
   fi
-  RAILS_ENV=test bundle exec bin/rake test TEST=test/models/upload_cleanup_job_test.rb
+  RAILS_ENV=test bundle exec bin/rake test
   code=$?
   if [[ $code -ne 0 ]]; then
     RETURN_CODE=$code
