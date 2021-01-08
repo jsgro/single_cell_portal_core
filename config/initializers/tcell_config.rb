812ea2be6fdf3f44fe319239201c85b02ebb8042
bistline <bistline@broadinstitute.org>
2021-01-08 14:44:04 -0500

    only log DEBUG in development for TCell

--- config/initializers/tcell_config.rb
+++ config/initializers/tcell_config.rb
@@ -3,6 +3,6 @@ if defined?(TCellAgent) && ENV['TCELL_AGENT_APP_ID'].present? && ENV['T_CELL_SER
   TCellAgent.configure do |config|
     config.app_id = "#{ENV['TCELL_AGENT_APP_ID']}"
     config.api_key = "#{ENV['T_CELL_SERVER_AGENT_API_KEY']}"
-    config.logging_options = {"enabled" => true, "level" => Rails.env == 'production' ? "INFO" : "DEBUG"}
+    config.logging_options = {"enabled" => true, "level" => Rails.env == 'development' ? "DEBUG" : "INFO"}
   end
 end
