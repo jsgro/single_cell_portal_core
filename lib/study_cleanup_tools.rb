98071199959fc8854bab8821d69fb3129a7337fd
bistline <bistline@broadinstitute.org>
2021-01-13 12:42:51 -0500

    refactoring re: PR comments about validations, method signatures

--- lib/study_cleanup_tools.rb
+++ lib/study_cleanup_tools.rb
@@ -19,18 +19,22 @@ module StudyCleanupTools
   #   - +allow_dev_env+: (Boolean) => allow execution of this method in development environment (default: false)
   #
   # * *raises*
-  #   - (ArgumentError) => if command cannot be run for failing a security check (hostname, project, environment, etc)
+  #   - (RuntimeError) => if command cannot be run for failing a security check (hostname, project, environment, etc)
   #
   # * *returns*
-  #   - (TrueClass) => true if method was performed, otherwise ArgumentError is thrown
+  #   - (Integer) => count of studies that were deleted
   def self.destroy_all_studies_and_workspaces(allow_dev_env: false)
-    if validate_environment!(allow_dev_env) && validate_hostname!
-      Study.all.each do |study|
-        # do not mass delete studies in protected projects
-        study.destroy_and_remove_workspace if permit_billing_project?(study.firecloud_project)
+    halt_on_validation_fail(:permit_environment?, allow_dev_env)
+    halt_on_validation_fail(:permit_hostname?)
+    study_count = 0
+    Study.all.each do |study|
+      # do not mass delete studies in protected projects
+      if permit_billing_project?(study.firecloud_project)
+        study.destroy_and_remove_workspace
+        study_count += 1
       end
-      true
     end
+    study_count
   end
 
   # delete all Terra workspaces in a project that do not have a study associated with them in the given env/schema
@@ -42,118 +46,115 @@ module StudyCleanupTools
   #   - +allow_dev_env+: (Boolean) => allow execution of this method in development environment (default: false)
   #
   # * *raises*
-  #   - (ArgumentError) => if command cannot be run for failing a security check (hostname, project, environment, etc)
+  #   - (RuntimeError) => if command cannot be run for failing a security check (hostname, project, environment, etc)
   #
   # * *returns*
-  #   - (TrueClass) => true if method was performed, otherwise ArgumentError is thrown
+  #   - (Integer) => count of workspaces that were deleted
   def self.delete_all_orphaned_workspaces(project_name = FireCloudClient::PORTAL_NAMESPACE, allow_dev_env: false)
-    if validate_hostname! && validate_environment!(allow_dev_env) && validate_billing_project!(project_name) && validate_continuous_integration!
-      workspaces = ApplicationController.firecloud_client.workspaces(project_name)
-      workspaces.each do |workspace|
-        ws_attr = workspace.dig('workspace')
-        ws_name = ws_attr['name']
-        ws_project = ws_attr['namespace']
-        ws_owner = ws_attr['createdBy']
-        existing_study = Study.find_by(firecloud_project: ws_project, firecloud_workspace: ws_name)
-        if existing_study.present?
-          puts "skipping #{ws_project}/#{ws_name} as it belongs to #{existing_study.accession}"
-          next
-        else
-          begin
-            if is_workspace_owner?(workspace)
-              print "deleting #{ws_project}/#{ws_name}... "
-              ApplicationController.firecloud_client.delete_workspace(ws_project, ws_name)
-              puts "#{ws_project}/#{ws_name} successfully deleted"
-            else
-              puts "skipping #{ws_project}/#{ws_name} because it was created by #{ws_owner}"
-            end
-          rescue => e
-            puts "Unable to delete #{ws_project}/#{ws_name} due to: #{e.class.name} - #{e.message}"
+    halt_on_validation_fail(:permit_environment?, allow_dev_env)
+    halt_on_validation_fail(:permit_hostname?)
+    halt_on_validation_fail(:permit_billing_project?, project_name)
+    halt_on_validation_fail(:is_continuous_integration?)
+    workspaces = ApplicationController.firecloud_client.workspaces(project_name)
+    workspace_count = 0
+    workspaces.each do |workspace|
+      ws_attr = workspace.dig('workspace')
+      ws_name = ws_attr['name']
+      ws_project = ws_attr['namespace']
+      ws_owner = ws_attr['createdBy']
+      existing_study = Study.find_by(firecloud_project: ws_project, firecloud_workspace: ws_name)
+      if existing_study.present?
+        puts "skipping #{ws_project}/#{ws_name} as it belongs to #{existing_study.accession}"
+        next
+      else
+        begin
+          if service_account_created_workspace?(workspace)
+            print "deleting #{ws_project}/#{ws_name}... "
+            ApplicationController.firecloud_client.delete_workspace(ws_project, ws_name)
+            workspace_count += 1
+            puts "#{ws_project}/#{ws_name} successfully deleted"
+          else
+            puts "skipping #{ws_project}/#{ws_name} because it was created by #{ws_owner}"
           end
+        rescue => e
+          puts "Unable to delete #{ws_project}/#{ws_name} due to: #{e.class.name} - #{e.message}"
         end
       end
-      true
     end
+    workspace_count
   end
 
   ##
   # security safeguards to prevent accidental deletes of production/staging workspaces and studies
   ##
 
+  # handler to run a security check and raise an error on returning false to halt execution
+  #
+  # * *params*
+  #   - +validation_name+: (String, Symbol) => name of security check to perform
+  #   - +args+: (*Array) => array of arguments for calling method, passed with splat (*)
+  #
+  # * *raises*
+  #   - (RuntimeError) => raise exception if security check returns false to halt parent method execution
+  def self.halt_on_validation_fail(validation_name, *validation_args)
+    # check for presence of arguments to avoid ArgumentError in calling method
+    valid_command = validation_args.any? ? self.send(validation_name, *validation_args) : self.send(validation_name)
+    if !valid_command
+      # report validation that failed and method caller
+      error_message = "halting execution as #{validation_name} failed validation when calling #{self.name}##{caller_locations.first.label}"
+      raise RuntimeError.new(error_message)
+    end
+  end
+
   # ensure commands cannot be run on deployed hosts
   #
-  # * *raises*
-  #   - (ArgumentError) => if hostname matches DISALLOWED_HOSTS
-  #
   # * *returns*
-  #   - (TrueClass) => true if hostname passes validation
-  def self.validate_hostname!
-    current_host = Socket.gethostname
-    if current_host =~ DISALLOWED_HOSTS
-      raise ArgumentError.new("#{current_host} is not a permitted host for running StudyCleanupTools methods")
-    else
-      true
-    end
+  #   - (Boolean) => true/false if hostname is permitted
+  def self.permit_hostname?
+    Socket.gethostname !~ DISALLOWED_HOSTS
   end
 
-  # ensure commands cannot be run against protected billing projects for production/staging
+  # ensure commands cannot be run in production/staging environments
+  # can be run manually in development if allow_dev_env is set to true
   #
-  # * *raises*
-  #   - (ArgumentError) => if billing project matches DISALLOWED_BILLING_PROJECTS, or is not the configured project for this instance
+  # * *params*
+  #   - +allow_dev_env+: (Boolean) => true/false to allow running command in development environment (defaults to false)
   #
-  # * *returns*
-  #   - (TrueClass) => true if billing project passes validation
-  def self.validate_billing_project!(project_name)
-    if !permit_billing_project?(project_name)
-      raise ArgumentError.new("#{project_name} is not a permitted billing project for running StudyCleanupTools methods")
+  # # * *returns*
+  #   - (Boolean) => true/false if environment is permitted
+  def self.permit_environment?(allow_dev_env = false)
+    if allow_dev_env
+      Rails.env.test? || Rails.env.pentest? || Rails.env.development?
     else
-      true
+      Rails.env.test? || Rails.env.pentest?
     end
   end
 
   # ensure commands cannot be run in production/staging environments
   # can be run manually in development if allow_dev_env is set to true
   #
-  # * *raises*
-  #   - (ArgumentError) => if environment is not test/pentest, or is development and allow_dev_env = false
-  #
   # * *returns*
-  #   - (TrueClass) => true if environment passes validation
-  def self.validate_environment!(allow_dev_env = false)
-    environments = %w(test pentest)
-    allow_dev_env ? environments << 'development' : nil
-    if !environments.include?(Rails.env)
-      raise ArgumentError.new("#{Rails.env} is not a permitted environment for running StudyCleanupTools methods")
-    else
-      true
-    end
+  #   - (Boolean) => true/false if this is a CI run
+  def self.is_continuous_integration?
+    !!(ENV['CI']) # use double-bang (!!) to convert nil to false
   end
 
-  # ensure commands cannot be run in production/staging environments
-  # can be run manually in development if allow_dev_env is set to true
+  # ensure requested billing project is not disallowed, and is the configured project for this instance
   #
-  # * *raises*
-  #   - (ArgumentError) => if this is not a continuous integration run (governed by ENV['CI'] == true)
+  # * *params*
+  #   - +project_name+: (String) => name of Terra billing project
   #
   # * *returns*
-  #   - (TrueClass) => true if CI run passes validation
-  def self.validate_continuous_integration!
-    if !ENV['CI'] || ENV['CI'].blank?
-      raise ArgumentError.new("This is not a continuous integration test run, ENV['CI'] is not true")
-    else
-      true
-    end
-  end
-
-  private
-
-  # is requested project permitted to run a command
+  #   - (Boolean) => true/false if requested projected is permitted
   def self.permit_billing_project?(project_name)
-    !DISALLOWED_BILLING_PROJECTS.include?(project_name) && project_name == FireCloudClient::PORTAL_NAMESPACE ? true : false
+    !!(!DISALLOWED_BILLING_PROJECTS.include?(project_name) && project_name == FireCloudClient::PORTAL_NAMESPACE)
   end
 
-  # is requested workspace created by this service account
-  def self.is_workspace_owner?(workspace_attributes)
-    ApplicationController.firecloud_client.issuer == workspace_attributes.dig('workspace', 'createdBy')
+  # is requested workspace created by portal service account
+  #
+  # * *returns*
+  #   - (Boolean) => true/false if workspace was created by configured service account
+  def self.service_account_created_workspace?(workspace_attributes)
+    ApplicationController.firecloud_client.issuer == workspace_attributes.with_indifferent_access.dig(:workspace, :createdBy)
   end
 end
--- test/lib/study_cleanup_tools_test.rb
+++ test/lib/study_cleanup_tools_test.rb
@@ -1,48 +1,86 @@
 require 'test_helper'
 
 # test all validations to ensure security is being enforced that will block accidental deletes of user studies/workspaces
+# does not directly test cleanup methods as these would destroy seed data needed for other tests
 class StudyCleanupToolsTest < ActiveSupport::TestCase
 
+  include Minitest::Hooks
+  include SelfCleaningSuite
   include TestInstrumentor
+
+  before(:all) do
+    @user = FactoryBot.create(:user, test_array: @@users_to_clean)
+    @basic_study = FactoryBot.create(:study, name_prefix: 'Cleanup Security Checks', test_array: @@studies_to_clean)
+  end
 
   teardown do
     Rails.env = "test" # reset this value to prevent other env checks failing later as this will persist across all runs
   end
 
   test 'should validate hostname' do
-    assert StudyCleanupTools.validate_hostname!
+    assert StudyCleanupTools.permit_hostname?
   end
 
   test 'should validate billing project' do
     billing_project = FireCloudClient::PORTAL_NAMESPACE
-    assert StudyCleanupTools.validate_billing_project!(billing_project)
+    assert StudyCleanupTools.permit_billing_project?(billing_project)
 
-    # ensure error is raised on validation fail
-    begin
-      bad_project = "this-is-not-valid"
-      refute StudyCleanupTools.validate_billing_project!(bad_project)
-    rescue ArgumentError => error
-      assert error.is_a?(ArgumentError)
-    end
+    bad_project = "this-is-not-valid"
+    refute StudyCleanupTools.permit_billing_project?(bad_project)
   end
 
   test 'should validate environment' do
-    assert StudyCleanupTools.validate_environment!
+    assert StudyCleanupTools.permit_environment?
 
     # test allowing development environment
     Rails.env = 'development'
-    assert StudyCleanupTools.validate_environment!(allow_dev_env: true)
+    assert StudyCleanupTools.permit_environment?(allow_dev_env: true)
 
-    # ensure error is raised on validation fail
-    begin
-      Rails.env = 'staging'
-      refute StudyCleanupTools.validate_environment!
-    rescue ArgumentError => error
-      assert error.is_a?(ArgumentError)
-    end
+    # ensure production-like environments are disallowed
+    Rails.env = 'staging'
+    refute StudyCleanupTools.permit_environment?
   end
 
   test 'should validate continuous integration' do
-    assert StudyCleanupTools.validate_continuous_integration!
+    assert StudyCleanupTools.is_continuous_integration?
+  end
+
+  test 'should match workspace creator for service account' do
+    study = Study.first
+    workspace = ApplicationController.firecloud_client.get_workspace(study.firecloud_project, study.firecloud_workspace)
+    assert StudyCleanupTools.service_account_created_workspace?(workspace)
+
+    # negative test with mock workspace JSON
+    not_owned_by_portal = {workspace: {createdBy: @user.email}}
+    refute StudyCleanupTools.service_account_created_workspace?(not_owned_by_portal)
+  end
+
+  test 'should raise error on validation failure' do
+
+    # positive tests - known goods should return nil as there is no declared return value
+    # no arguments
+    assert_nil StudyCleanupTools.halt_on_validation_fail(:permit_environment?)
+
+    # with arguments
+    assert_nil StudyCleanupTools.halt_on_validation_fail(:permit_billing_project?, FireCloudClient::PORTAL_NAMESPACE)
+
+    # negative test with no arguments - should throw RuntimeError
+    begin
+      Rails.env = 'staging'
+      StudyCleanupTools.halt_on_validation_fail(:permit_environment?)
+    rescue RuntimeError => error
+      assert error.is_a?(RuntimeError)
+      assert error.message.include?('permit_environment?'),
+             "Did not find validation method signature for :permit_environment? in message: #{error.message}"
+    end
+
+    # negative test with arguments
+    begin
+      StudyCleanupTools.halt_on_validation_fail(:permit_billing_project?, 'invalid-project')
+    rescue RuntimeError => error
+      assert error.is_a?(RuntimeError)
+      assert error.message.include?('permit_billing_project?'),
+             "Did not find method signature for :permit_billing_project? in message: #{error.message}"
+    end
   end
 end
