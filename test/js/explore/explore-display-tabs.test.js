4b97fc1f074a7816641adeda55311ef228e09f36
bistline <bistline@broadinstitute.org>
2021-03-10 12:45:13 -0500

    fixing test imports/mocks, restructuring tests for coverage

--- test/js/explore/explore-display-tabs.test.js
+++ test/js/explore/explore-display-tabs.test.js
@@ -1,7 +1,63 @@
+// mock various modules from genome tab as these aren't being used, and throw compilation errors from jest
+jest.mock('components/explore/GenomeView', () => {
+    return {
+        igv: jest.fn(() => mockPromise)
+    }
+})
+
+jest.mock('components/visualization/RelatedGenesIdeogram', () => {
+    return {
+        Ideogram: jest.fn(() => mockPromise)
+    }
+})
+
 import { getEnabledTabs } from 'components/explore/ExploreDisplayTabs'
 
-describe("explore tabs are activated based on variables", () => {
-    it('should enable tabs based on exploreInfo and exploreParams', async () => {
+// function to wrap test assertions
+function assertTabConfig(exploreInfo, exploreParams, expectedResults) {
+    const {enabledTabs, isGeneList, isGene, isMultiGene} = getEnabledTabs(exploreInfo, exploreParams)
+    expect(enabledTabs).toEqual(expectedResults.enabledTabs)
+    expect(isGeneList).toEqual(expectedResults.isGeneList)
+    expect(isGene).toEqual(expectedResults.isGene)
+    expect(isMultiGene).toEqual(expectedResults.isMultiGene)
+}
+
+describe("explore tabs are activated based on study info and parameters", () => {
+    it('should enable cluster tab', async () => {
+        // mock exploreInfo from study
+        const exploreInfo = {
+            cluster: 'foo',
+            taxonNames: ['Homo sapiens'],
+            inferCNVIdeogramFiles: [],
+            bamBundleList: [],
+            uniqueGenes: ['Agpat2', 'Apoe', 'Gad1', 'Gad2'],
+            geneLists: [],
+            annotationList: [],
+            clusterGroupNames: ['foo', 'bar'],
+            spatialGroupNames: [],
+            spatialGroups: [],
+            clusterPointAlpha: 1.0
+        }
+
+        const exploreParams = {
+            cluster: 'foo', // request params loading only a cluster
+            annotation: { name: 'bar', type: 'group', scope: 'study' },
+            userSpecified: {
+                annotation: true,
+                cluster: true
+            }
+        }
+        const expectedResults = {
+            enabledTabs: ['cluster'],
+            isGeneList: false,
+            isGene: false,
+            isMultiGene: false
+        }
+
+        assertTabConfig(exploreInfo, exploreParams, expectedResults)
+    })
+
+    it('should enable cluster and genome tab', async () => {
         // mock exploreInfo from study
         const exploreInfo = {
             cluster: 'foo',
@@ -12,7 +68,146 @@ describe("explore tabs are activated based on variables", () => {
                 {"name": "sample1.bam.bai", "file_type": "BAM Index"}
             ],
             uniqueGenes: ['Agpat2', 'Apoe', 'Gad1', 'Gad2'],
+            geneLists: [],
+            annotationList: [],
+            clusterGroupNames: ['foo', 'bar'],
+            spatialGroupNames: [],
+            spatialGroups: [],
+            clusterPointAlpha: 1.0
+        }
+
+        const exploreParams = {
+            cluster: 'foo',
+            annotation: { name: 'bar', type: 'group', scope: 'study' },
+            bamFileName: 'sample1.bam',
+            userSpecified: {
+                annotation: true,
+                cluster: true,
+                bamFileName: true
+            }
+        }
+        const expectedResults = {
+            enabledTabs: ['cluster', 'genome'],
+            isGeneList: false,
+            isGene: false,
+            isMultiGene: false
+        }
+
+        assertTabConfig(exploreInfo, exploreParams, expectedResults)
+    })
+
+    it('should enable heatmap tab for gene lists', async () => {
+        // mock exploreInfo from study
+        const exploreInfo = {
+            cluster: 'foo',
+            taxonNames: ['Homo sapiens'],
+            inferCNVIdeogramFiles: [],
+            bamBundleList: [],
+            uniqueGenes: ['Agpat2', 'Apoe', 'Gad1', 'Gad2'],
             geneLists: ['Gene List 1', 'Gene List 2'],
+            annotationList: [],
+            clusterGroupNames: ['foo', 'bar'],
+            spatialGroupNames: [],
+            spatialGroups: [],
+            clusterPointAlpha: 1.0
+        }
+
+        const exploreParams = {
+            geneList: 'Gene List 1',
+            userSpecified: {
+                geneList: true
+            }
+        }
+        const expectedResults = {
+            enabledTabs: ['heatmap'],
+            isGeneList: true,
+            isGene: false,
+            isMultiGene: false
+        }
+
+        assertTabConfig(exploreInfo, exploreParams, expectedResults)
+    })
+
+    it ('should enable scatter/distribution tabs when searching one gene', async () => {
+        const exploreInfo = {
+            cluster: 'foo',
+            taxonNames: ['Homo sapiens'],
+            inferCNVIdeogramFiles: [],
+            bamBundleList: [],
+            uniqueGenes: ['Agpat2', 'Apoe', 'Gad1', 'Gad2'],
+            geneLists: [],
+            annotationList: [],
+            clusterGroupNames: ['foo', 'bar'],
+            spatialGroupNames: [],
+            spatialGroups: [],
+            clusterPointAlpha: 1.0
+        }
+
+        const exploreParams = {
+            cluster: 'foo',
+            genes: ['Agpat2'],
+            annotation: {name: 'bar', type: 'group', scope: 'study'},
+            userSpecified: {
+                annotation: true,
+                cluster: true,
+                genes: true
+            }
+        }
+
+        const expectedResults = {
+            enabledTabs: ['scatter', 'distribution'],
+            isGeneList: false,
+            isGene: true,
+            isMultiGene: false
+        }
+
+        assertTabConfig(exploreInfo, exploreParams, expectedResults)
+    })
+
+    it ('should enable dotplot/heatmap tabs when searching multiple genes', async () => {
+        const exploreInfo = {
+            cluster: 'foo',
+            taxonNames: ['Homo sapiens'],
+            inferCNVIdeogramFiles: [],
+            bamBundleList: [],
+            uniqueGenes: ['Agpat2', 'Apoe', 'Gad1', 'Gad2'],
+            geneLists: [],
+            annotationList: [],
+            clusterGroupNames: ['foo', 'bar'],
+            spatialGroupNames: [],
+            spatialGroups: [],
+            clusterPointAlpha: 1.0
+        }
+
+        const exploreParams = {
+            cluster: 'foo',
+            genes: ['Agpat2', 'Apoe'],
+            annotation: {name: 'bar', type: 'group', scope: 'study'},
+            userSpecified: {
+                annotation: true,
+                cluster: true,
+                genes: true
+            }
+        }
+
+        const expectedResults = {
+            enabledTabs: ['dotplot', 'heatmap'],
+            isGeneList: false,
+            isGene: true,
+            isMultiGene: true
+        }
+
+        assertTabConfig(exploreInfo, exploreParams, expectedResults)
+    })
+
+    it ('should enable spatial/dotplot/heatmap tabs when searching multiple genes', async () => {
+        const exploreInfo = {
+            cluster: 'foo',
+            taxonNames: ['Homo sapiens'],
+            inferCNVIdeogramFiles: [],
+            bamBundleList: [],
+            uniqueGenes: ['Agpat2', 'Apoe', 'Gad1', 'Gad2'],
+            geneLists: [],
             annotationList: [],
             clusterGroupNames: ['foo', 'bar'],
             spatialGroupNames: ['bing', 'baz'],
@@ -23,134 +218,64 @@ describe("explore tabs are activated based on variables", () => {
             clusterPointAlpha: 1.0
         }
 
-        // Construct multiple mock parameter objects and the expected results from getEnabledTabs
-        // this is used to through each pair of params/results and asserts returns
-        // while this looks weird it reduces the amount of repeated assertions
-        const testParamsAndResults = [
-            [
-                {
-                    cluster: 'foo', // request params loading only a cluster
-                    annotation: { name: 'bar', type: 'group', scope: 'study' },
-                    userSpecified: {
-                        annotation: true,
-                        cluster: true
-                    }
-                }, {
-                enabledTabs: ['cluster'],
-                isGeneList: false,
-                isGene: false,
-                isMultiGene: false
+        const exploreParams = {
+            cluster: 'foo',
+            genes: ['Agpat2', 'Apoe'],
+            annotation: { name: 'bar', type: 'group', scope: 'study' },
+            spatialGroups: ['square', 'circle'],
+            userSpecified: {
+                annotation: true,
+                cluster: true,
+                genes: true,
+                spatialGroups: true
             }
-            ],
-            [
-                {
-                    cluster: 'foo', // request params loading a cluster and one gene
-                    genes: ['agpat2'],
-                    annotation: { name: 'bar', type: 'group', scope: 'study' },
-                    userSpecified: {
-                        annotation: true,
-                        cluster: true,
-                        genes: true
-                    }
-                }, {
-                enabledTabs: ['dotplot', 'heatmap'],
-                isGeneList: false,
-                isGene: false,
-                isMultiGene: false
-            }
-            ],
-            [
-                {
-                    cluster: 'foo', // request params for cluster and spatial w/ one gene
-                    genes: ['agpat2'],
-                    annotation: {name: 'bar', type: 'group', scope: 'study'},
-                    spatialGroups: ['square', 'circle'],
-                    userSpecified: {
-                        annotation: true,
-                        cluster: true,
-                        genes: true,
-                        spatialGroups: true
-                    }
-                }, {
-                enabledTabs: ['scatter', 'distribution'],
-                isGeneList: false,
-                isGene: true,
-                isMultiGene: false
-            }
-            ],
-            [
-                {
-                    cluster: 'foo', // request params loading a cluster and multiple genes
-                    genes: ['agpat2', 'apoe'],
-                    annotation: { name: 'bar', type: 'group', scope: 'study' },
-                    userSpecified: {
-                        annotation: true,
-                        bamFileName: true,
-                        cluster: true,
-                        genes: true,
-                        geneList: true,
-                        spatialGroups: true
-                    }
-                }, {
-                enabledTabs: ['spatial', 'dotplot', 'heatmap'],
-                isGeneList: false,
-                isGene: true,
-                isMultiGene: true
-            }
-            ],
-            [
-                {
-                    cluster: 'foo',  // request params loading a cluster and multiple genes w/ consensus
-                    genes: ['agpat2', 'apoe'],
-                    annotation: { name: 'bar', type: 'group', scope: 'study' },
-                    consensus: 'mean',
-                    userSpecified: {
-                        annotation: true,
-                        cluster: true,
-                        genes: true,
-                        consensus: true
-                    }
-                }, {
-                enabledTabs: ['scatter', 'distribution', 'dotplot'],
-                isGeneList: false,
-                isGene: true,
-                isMultiGene: true
-            }
-            ],
-            [
-                {
-                    bamFileName: 'sample1.bam', // request params for bam file only
-                    userSpecified: {
-                        bamFileName: true
-                    }
-                }, {
-                enabledTabs: ['genome'],
-                isGeneList: false,
-                isGene: false,
-                isMultiGene: false
-            }
-            ],
-            [
-                {
-                    geneList: 'Gene List 1', // request params for gene list only
-                    userSpecified: {
-                        geneList: true
-                    }
-                }, {
-                enabledTabs: ['heatmap'],
-                isGeneList: true,
-                isGene: false,
-                isMultiGene: false
-            }
-            ]
-        ]
-
-        for ( var [exploreParams, expectedResults] of testParamsAndResults) {
-            let {enabledTabs, isGeneList, isGene, isMultiGene} = getEnabledTabs(exploreInfo, exploreParams)
-            expect(enabledTabs).toEqual(expectedResults.enabledTabs)
-            expect(isGeneList).toEqual(expectedResults.isGeneList)
-            expect(isGene).toEqual(expectedResults.isGene)
-            expect(isMultiGene).toEqual(expectedResults.isMultiGene)
         }
+
+        const expectedResults = {
+            enabledTabs: ['spatial', 'dotplot', 'heatmap'],
+            isGeneList: false,
+            isGene: true,
+            isMultiGene: true
+        }
+
+        assertTabConfig(exploreInfo, exploreParams, expectedResults)
+    })
+
+    it ('should enable scatter/distribution/dotplot tabs when searching multiple genes w/ consensus', async () => {
+        const exploreInfo = {
+            cluster: 'foo',
+            taxonNames: ['Homo sapiens'],
+            inferCNVIdeogramFiles: [],
+            bamBundleList: [],
+            uniqueGenes: ['Agpat2', 'Apoe', 'Gad1', 'Gad2'],
+            geneLists: [],
+            annotationList: [],
+            clusterGroupNames: ['foo', 'bar'],
+            spatialGroupNames: [],
+            spatialGroups: [],
+            clusterPointAlpha: 1.0
+        }
+
+        const exploreParams = {
+            cluster: 'foo',
+            genes: ['Agpat2', 'Apoe'],
+            annotation: { name: 'bar', type: 'group', scope: 'study' },
+            consensus: 'mean',
+            userSpecified: {
+                annotation: true,
+                cluster: true,
+                genes: true,
+                consensus: true
+            }
+        }
+
+        const expectedResults = {
+            enabledTabs: ['scatter', 'distribution', 'dotplot'],
+            isGeneList: false,
+            isGene: true,
+            isMultiGene: true
+        }
+
+        assertTabConfig(exploreInfo, exploreParams, expectedResults)
     })
 })
