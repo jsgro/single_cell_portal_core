3007ada39cef7c158df372d6b5d1daa4f10001c4
bistline <bistline@broadinstitute.org>
2021-04-02 18:03:32 -0400

    changing namespace for RealIpLogger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 
 class ApplicationController < ActionController::Base
 
-  include Concerns::RealIpLogger
+  include RealIpLogger
 
   ###
   #
--- app/controllers/concerns/real_ip_logger.rb
+++ app/controllers/concerns/real_ip_logger.rb
@@ -1,23 +1,21 @@
-module Concerns
-  module RealIpLogger
-    ##
-    # real_ip_logger.rb: logs the actual client IP address set by the nginx X-Real-Ip header, instead of load balancer ip
-    # see http://nginx.org/en/docs/http/ngx_http_realip_module.html for more info on associated nginx module
-    # and webapp.conf for configuration
-    ##
+module RealIpLogger
+  ##
+  # real_ip_logger.rb: logs the actual client IP address set by the nginx X-Real-Ip header, instead of load balancer ip
+  # see http://nginx.org/en/docs/http/ngx_http_realip_module.html for more info on associated nginx module
+  # and webapp.conf for configuration
+  ##
 
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    included do
-      around_action :log_real_ip
-    end
+  included do
+    around_action :log_real_ip
+  end
 
-    def log_real_ip
-      real_ip = request.headers['HTTP_X_REAL_IP']
-      Rails.logger.info "Real client IP: #{real_ip} for #{request.method} #{request.fullpath}" if real_ip.present?
-    ensure
-      yield # regardless of any issues/errors, request will always execute and render a response
-    end
+  def log_real_ip
+    real_ip = request.headers['HTTP_X_REAL_IP']
+    Rails.logger.info "Real client IP: #{real_ip} for #{request.method} #{request.fullpath}" if real_ip.present?
+  ensure
+    yield # regardless of any issues/errors, request will always execute and render a response
   end
 end
 
