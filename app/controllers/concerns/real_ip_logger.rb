5f301128003e01a48df529dee71a7c28a10da04f
bistline <bistline@broadinstitute.org>
2021-04-05 11:06:23 -0400

    changing back to X-Forwarded-For

--- app/controllers/concerns/real_ip_logger.rb
+++ app/controllers/concerns/real_ip_logger.rb
@@ -12,11 +12,8 @@ module RealIpLogger
   end
 
   def log_real_ip
-    real_ip = request.headers['HTTP_X_REAL_IP']
-    Rails.logger.info "Real client IP: #{real_ip} for #{request.method} #{request.fullpath}" if real_ip.present?
-    request.headers.each do |header|
-      Rails.logger.info "request header: #{header}"
-    end
+    real_ip = request.headers['HTTP_X_FORWARDED_FOR']
+    Rails.logger.info "Forwarded client IP: #{real_ip} for #{request.method} #{request.fullpath}" if real_ip.present?
   ensure
     yield # regardless of any issues/errors, request will always execute and render a response
   end
