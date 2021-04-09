96d98515ca4052f16066cd065f2257246fc12390
bistline <bistline@broadinstitute.org>
2021-04-09 14:48:07 -0400

    validate billing scope checks via auth provider

---
+++ test/controllers/billing_projects_controller_test.rb
@@ -0,0 +1,44 @@
+require "integration_test_helper"
+require 'minitest/mock'
+
+class BillingProjectsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @test_user = User.find_by(email: 'testing.user@gmail.com')
+    sign_in @test_user
+    auth_as_user @test_user
+  end
+
+  teardown do
+    OmniAuth.config.mock_auth[:google] = nil
+    OmniAuth.config.mock_auth[:google_billing] = nil
+  end
+
+  test 'should reauthenticate user when accessing billing projects' do
+    puts "#{File.basename(__FILE__)}: #{self.method_name}"
+
+    # ensure users are redirected to access request page that explains need for cloud-billing.readonly scope
+    get billing_projects_path
+    assert_response 302
+    assert_redirected_to billing_projects_access_request_path
+
+    # mock user authenticating with cloud-billing scope
+    auth_as_user(@test_user, :google_billing)
+
+    billing_projects_mock = Minitest::Mock.new
+
+    # mock all service/permission checks that happen when loading billing projects page
+    billing_projects_mock.expect :services_available?, true, [FireCloudClient::THURLOE_SERVICE]
+    billing_projects_mock.expect :storage_issuer, String
+    billing_projects_mock.expect :get_billing_accounts, []
+    billing_projects_mock.expect :get_billing_projects, []
+    FireCloudClient.stub :new, billing_projects_mock do
+      get billing_projects_path
+      assert_response 200
+      billing_projects_mock.verify
+    end
+
+    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
+  end
+end
