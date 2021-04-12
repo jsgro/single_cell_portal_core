b26bf5d98614a6de67c1a61dbadba8cc691b97d8
Devon Bush <dbush@broadinstitute.org>
2021-04-12 18:26:49 -0400

    removing byebug

--- app/lib/bulk_download_service.rb
+++ app/lib/bulk_download_service.rb
@@ -48,7 +48,6 @@ class BulkDownloadService
       manifest_config += "output=\"#{study.accession}/file_supplemental_info.tsv\""
       curl_configs << manifest_config
     end
-    byebug
     MetricsService.log('file-download:curl-config', {
       numFiles: study_files.count,
       numStudies: studies.count,
