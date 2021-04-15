efb0a7bce97fa6292159ae3b5f672b51b241ca1e
Devon Bush <dbush@broadinstitute.org>
2021-04-15 11:58:35 -0400

    adding checkusm validation to codecov uploader

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -138,7 +138,14 @@ if [[ $code -ne 0 ]]; then
 fi
 if [[ "$CODECOV_TOKEN" != "" ]] && [[ "$CI" == "true" ]]; then
   echo "uploading all coverage data to codecov"
-  bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
+  # code below adapted from https://docs.codecov.io/docs/about-the-codecov-bash-uploader
+  curl -s https://codecov.io/bash > codecov
+  CODECOV_VERSION=$(grep 'VERSION=\".*\"' codecov | cut -d'"' -f2);
+  for i in 1 256 512
+  do
+    diff <(shasum -a $i codecov) <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/$CODECOV_VERSION/SHA${i}SUM)  || { echo "CodeCov checksum validation failed!" >&2; exit 1; }
+  done
+  bash <codecov -t $CODECOV_TOKEN
 fi
 
 clean_up
