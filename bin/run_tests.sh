9d2f6e5fdb3ba7f08b2c574962625e5ff5aa1657
Devon <dbush@broadinstitute.org>
2021-04-15 12:53:22 -0400

    using Broad-centralized validator script
    Co-authored-by: Eric Weitz <eweitz@broadinstitute.org>

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -138,14 +138,7 @@ if [[ $code -ne 0 ]]; then
 fi
 if [[ "$CODECOV_TOKEN" != "" ]] && [[ "$CI" == "true" ]]; then
   echo "uploading all coverage data to codecov"
-  # code below adapted from https://docs.codecov.io/docs/about-the-codecov-bash-uploader
-  curl -s https://codecov.io/bash > codecov
-  CODECOV_VERSION=$(grep 'VERSION=\".*\"' codecov | cut -d'"' -f2);
-  for i in 1 256 512
-  do
-    diff <(shasum -a $i codecov) <(curl -s https://raw.githubusercontent.com/codecov/codecov-bash/$CODECOV_VERSION/SHA${i}SUM)  || { echo "CodeCov checksum validation failed!" >&2; exit 1; }
-  done
-  bash <codecov -t $CODECOV_TOKEN
+  bash <(curl -s https://raw.githubusercontent.com/broadinstitute/codecov-bash-uploader/main/codecov-verified.bash)
 fi
 
 clean_up
