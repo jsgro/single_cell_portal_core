33ba9c95d2a942c7f5170c7f594567efa4bd691a
Eric Weitz <eweitz@broadinstitute.org>
2021-04-27 16:04:06 -0400

    Fix docstring for fetchSearch

--- app/javascript/lib/scp-api.js
+++ app/javascript/lib/scp-api.js
@@ -467,19 +467,15 @@ export async function fetchDownloadSize(accessions, fileTypes, mock=false) {
  *
  * Docs: https:///singlecell.broadinstitute.org/single_cell/api/swagger_docs/v1#!/Search/search
  *
- * @param {String} type Type of query to perform (study- or cell-based)
+ * @param {String} type Type of query to perform ("study" or "gene")
  * @param {Object} searchParams  Search parameters, including
  *   @param {String} terms Searched keywords
  *   @param {Object} facets Applied facets and filters
  *   @param {Integer} page Page in search results
  *   @param {String} order Results ordering field
- *   @param {String} preset_search Query preset (e.g. 'covid19')
+ *   @param {String} preset_search Query preset (e.g. "covid19")
  * @param {Boolean} mock If using mock data
  * @returns {Promise} Promise object containing camel-cased data from API
- *
- * @example
- *
- * fetchSearch('study', 'tuberculosis');
  */
 export async function fetchSearch(type, searchParams, mock=false) {
   const path = `/search?${buildSearchQueryString(type, searchParams)}`
