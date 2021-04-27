e0c97f9cd00cec90330ba7d53737abcd9b0ee548
bistline <bistline@broadinstitute.org>
2021-04-27 10:20:22 -0400

    refining regexp, update filters in Mongo after migration runs

--- db/migrate/20210426191308_replace10_x_in_alexandria_convention.rb
+++ db/migrate/20210426191308_replace10_x_in_alexandria_convention.rb
@@ -9,12 +9,14 @@ class Replace10XInAlexandriaConvention < Mongoid::Migration
         # query will match all instances of library_preparation_protocol__ontology_label beginning with the
         # substring '10X ' and replace it with '10x ', e.g. "10X 3' v2 sequencing" => "10x 3' v2 sequencing"
         update_query = "UPDATE #{CellMetadatum::BIGQUERY_TABLE}"
-        replace_clause = "SET #{col_name} = REGEXP_REPLACE(#{col_name}, r\"10X \", \"10x \")"
+        replace_clause = "SET #{col_name} = REGEXP_REPLACE(#{col_name}, r\"^10X \", \"10x \")"
         where_clause = "WHERE REGEXP_CONTAINS(#{col_name}, r\"^10X \")"
         query_string = "#{update_query} #{replace_clause} #{where_clause}"
         dataset.query query_string
       end
     end
+    # update cached entries
+    SearchFacet.update_all_facet_filters
   end
 
   def self.down
