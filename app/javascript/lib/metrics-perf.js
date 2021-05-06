6f0327f9076df20f3f790fbc6495775c3a1aa73f
Eric Weitz <eweitz@broadinstitute.org>
2021-05-06 09:43:37 -0400

    Use no-op in case of metrics error, per review

--- app/javascript/lib/metrics-perf.js
+++ app/javascript/lib/metrics-perf.js
@@ -216,6 +216,7 @@ export function calculatePerfTimes(perfTimes) {
     )
     console.error(message)
     log('metrics-error', { message })
+    return perfTimes
   }
 
   perfProps['perfTime:url'] = perfTimes.url
