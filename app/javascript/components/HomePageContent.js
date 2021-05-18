bc3e5d3d1ad806c860a749f5b1c69d657a8b5659
Eric Weitz <eweitz@broadinstitute.org>
2021-05-18 16:15:04 -0400

    Split XDS flag in two, frontend and backend, per review

--- app/javascript/components/HomePageContent.js
+++ app/javascript/components/HomePageContent.js
@@ -7,7 +7,7 @@ import { getFeatureFlagsWithDefaults } from 'providers/UserProvider'
 /** Until refactor is done, this merely sets appropriate component */
 export default function HomePageContent() {
   const flags = getFeatureFlagsWithDefaults()
-  if (flags.cross_dataset_search) {
+  if (flags.cross_dataset_search_frontend) {
     return (<HomePageContentXds />)
   } else {
     return (<HomePageContentLegacy />)
