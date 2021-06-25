54d81b638f50a45872b9d7e570ace791ba8af66e
Eric Weitz <eweitz@broadinstitute.org>
2021-06-25 16:32:20 -0400

    Fix advanced search footer spacing

--- app/javascript/components/search/results/ResultsPanel.js
+++ app/javascript/components/search/results/ResultsPanel.js
@@ -81,11 +81,10 @@ const FacetResultsFooter = ({ studySearchState }) => {
           <p>Our advanced search is metadata-powered.
           By selecting filters, your search <b>targets only studies that use ontology terms</b> in their metadata file.
           Currently, about 20% of public studies supply that metadata.</p>
-          Learn more about our search capability on our
+          Learn more about our search capability on our{' '}
           <a href="https://github.com/broadinstitute/single_cell_portal/wiki/Search-Studies"
             target="_blank" rel="noreferrer">wiki
-          </a>.<br/>
-          Study authors looking to make their studies more accessible can read our
+          </a>.  Study authors looking to make their studies more accessible can read our{' '}
           {/* eslint-disable-next-line max-len */}
           <a href="https://github.com/broadinstitute/single_cell_portal/wiki/Metadata-File#Metadata-powered-Advanced-Search"
             target="_blank" rel="noreferrer">metadata guide
