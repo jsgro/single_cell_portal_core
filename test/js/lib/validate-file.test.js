9566d8fc6ba04a0ba0e9f2a4edaaa9ecd0fa3690
Eric Weitz <eweitz@broadinstitute.org>
2021-09-13 21:23:17 -0400

    Simplify in-app file validation test

--- test/js/lib/validate-file.test.js
+++ test/js/lib/validate-file.test.js
@@ -67,63 +67,29 @@ describe('Client-side file validation', () => {
   })
 
   it('catches duplicate headers', async () => {
-    const mockPath = `${mockDir}/dup_headers_v2.0.0.tsv`
-    mockReadLinesAndType(mockPath)
+    mockReadLinesAndType(`${mockDir}/dup_headers_v2.0.0.tsv`)
 
-    const file = {
-      name: 'dup_headers_v2.0.0.tsv',
-      size: 555,
-      type: 'text/plain'
-    }
-    const fileType = 'metadata'
+    const { errors, summary } = await ValidateFile.validateFile({}, 'metadata')
 
-    const expectedSummary = 'Your metadata file had 1 error'
-
-    const { errors, summary } = await ValidateFile.validateFile(file, fileType)
-
-    // Test library
     expect(errors).toHaveLength(1)
-    expect(summary).toBe(expectedSummary)
+    expect(summary).toBe('Your metadata file had 1 error')
   })
 
   it('catches mismatched header counts', async () => {
-    const mockPath = `${mockDir}/header_count_mismatch.tsv`
-    mockReadLinesAndType(mockPath)
+    mockReadLinesAndType(`${mockDir}/header_count_mismatch.tsv`)
+    const { errors, summary } = await ValidateFile.validateFile({}, 'metadata')
 
-    const file = {
-      name: 'header_count_mismatch.tsv',
-      size: 555,
-      type: 'text/tab-separated-values'
-    }
-    const fileType = 'metadata'
-
-    const expectedSummary = 'Your metadata file had 1 error'
-
-    const { errors, summary } = await ValidateFile.validateFile(file, fileType)
-
-    // Test library
     expect(errors).toHaveLength(1)
-    expect(summary).toBe(expectedSummary)
+    expect(summary).toBe('Your metadata file had 1 error')
   })
 
   it('catches multiple header errors', async () => {
-    const mockPath = `${mockDir}/error_headers_v2.0.0.tsv`
-    mockReadLinesAndType(mockPath)
+    mockReadLinesAndType(`${mockDir}/error_headers_v2.0.0.tsv`)
 
-    const file = {
-      name: 'error_headers_v2.0.0.tsv',
-      size: 555,
-      type: 'text/plain'
-    }
-    const fileType = 'metadata'
+    const { errors, summary } = await ValidateFile.validateFile({}, 'metadata')
 
-    const expectedSummary = 'Your metadata file had 3 errors'
-
-    const { errors, summary } = await ValidateFile.validateFile(file, fileType)
-
-    // Test library
     expect(errors).toHaveLength(3)
-    expect(summary).toBe(expectedSummary)
+    expect(summary).toBe('Your metadata file had 3 errors')
   })
 
   it('reports error for true positive of no coordinates in cluster file', async () => {
@@ -131,19 +97,10 @@ describe('Client-side file validation', () => {
     //
     // eslint-disable-next-line max-len
     // https://github.com/broadinstitute/scp-ingest-pipeline/blob/af1c124993f4a3e953debd5a594124f1ac52eee7/tests/test_cluster.py#L9
-    const mockPath = `${mockDir}/cluster_bad_no_coordinates.txt`
-    mockReadLinesAndType(mockPath)
+    mockReadLinesAndType(`${mockDir}/cluster_bad_no_coordinates.txt`)
 
-    const file = {
-      name: 'cluster_bad_no_coordinates.txt',
-      size: 555,
-      type: 'text/plain'
-    }
-    const fileType = 'cluster'
+    const { errors } = await ValidateFile.validateFile({}, 'cluster')
 
-    const { errors } = await ValidateFile.validateFile(file, fileType)
-
-    // Test library
     expect(errors).toHaveLength(0)
   })
 
@@ -155,16 +112,8 @@ describe('Client-side file validation', () => {
     const mockPath = `${mockDir}/cluster_example.txt`
     mockReadLinesAndType(mockPath)
 
-    const file = {
-      name: 'cluster_example.txt',
-      size: 555,
-      type: 'text/plain'
-    }
-    const fileType = 'cluster'
+    const { errors } = await ValidateFile.validateFile({}, 'cluster')
 
-    const { errors } = await ValidateFile.validateFile(file, fileType)
-
-    // Test library
     expect(errors).toHaveLength(0)
   })
 
@@ -176,16 +125,8 @@ describe('Client-side file validation', () => {
     const mockPath = `${mockDir}/metadata_bad_has_coordinates.txt`
     mockReadLinesAndType(mockPath)
 
-    const file = {
-      name: 'metadata_bad_has_coordinates.txt',
-      size: 555,
-      type: 'text/plain'
-    }
-    const fileType = 'cluster'
+    const { errors } = await ValidateFile.validateFile({}, 'metadata')
 
-    const { errors } = await ValidateFile.validateFile(file, fileType)
-
-    // Test library
     expect(errors).toHaveLength(0)
   })
 
@@ -194,19 +135,10 @@ describe('Client-side file validation', () => {
     //
     // eslint-disable-next-line max-len
     // https:// github.com/broadinstitute/scp-ingest-pipeline/blob/af1c124993f4a3e953debd5a594124f1ac52eee7/tests/test_cell_metadata.py#L31
-    const mockPath = `${mockDir}/metadata_example.txt`
-    mockReadLinesAndType(mockPath)
+    mockReadLinesAndType(`${mockDir}/metadata_example.txt`)
 
-    const file = {
-      name: 'metadata_example.txt',
-      size: 555,
-      type: 'text/plain'
-    }
-    const fileType = 'cluster'
+    const { errors } = await ValidateFile.validateFile({}, 'metadata')
 
-    const { errors } = await ValidateFile.validateFile(file, fileType)
-
-    // Test library
     expect(errors).toHaveLength(0)
   })
 
