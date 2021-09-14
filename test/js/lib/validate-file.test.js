6e5f6efba7db60f74c737c6d901f5499c485f455
Eric Weitz <eweitz@broadinstitute.org>
2021-09-13 21:32:46 -0400

    Further simplify in-app file validation tests

--- test/js/lib/validate-file.test.js
+++ test/js/lib/validate-file.test.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-
 import * as Io from 'lib/validation/io'
-import * as ValidateFile from 'lib/validation/validate-file'
+import { validateFile } from 'lib/validation/validate-file'
 import ValidationAlert from 'components/validation/ValidationAlert'
 import * as MetricsApi from 'lib/metrics-api'
 
 const fs = require('fs')
 
-const mockDir = 'public/mock_data/validation'
+const mockDir = 'public/mock_data/validation/'
 
-/** Mock function that uses FileReader, which isn't available in Node */
-function mockReadLinesAndType(mockPath) {
-  const fileContent = fs.readFileSync(mockPath, 'utf8')
+/** Loosely mock function that uses FileReader, as it's unavailable in Node */
+function mockReadLinesAndType(mockFileName) {
+  const fileContent = fs.readFileSync(mockDir + mockFileName, 'utf8')
 
   /** Mock function that uses FileReader, which isn't available in Node */
   const readLinesAndType = jest.spyOn(Io, 'readLinesAndType')
@@ -26,9 +25,7 @@ function mockReadLinesAndType(mockPath) {
 
 describe('Client-side file validation', () => {
   it('catches and logs errors via library interface', async () => {
-    const mockPath = `${mockDir}/metadata_bad_type_header.txt`
-
-    mockReadLinesAndType(mockPath)
+    mockReadLinesAndType('metadata_bad_type_header.txt')
 
     const file = {
       name: 'metadata_bad_type_header.txt',
@@ -42,7 +39,7 @@ describe('Client-side file validation', () => {
 
     const expectedSummary = 'Your metadata file had 1 error'
 
-    const { errors, summary } = await ValidateFile.validateFile(file, fileType)
+    const { errors, summary } = await validateFile(file, fileType)
 
     // Test library
     expect(errors).toHaveLength(1)
@@ -67,27 +64,22 @@ describe('Client-side file validation', () => {
   })
 
   it('catches duplicate headers', async () => {
-    mockReadLinesAndType(`${mockDir}/dup_headers_v2.0.0.tsv`)
-
-    const { errors, summary } = await ValidateFile.validateFile({}, 'metadata')
-
+    mockReadLinesAndType('dup_headers_v2.0.0.tsv')
+    const { errors, summary } = await validateFile({}, 'metadata')
     expect(errors).toHaveLength(1)
     expect(summary).toBe('Your metadata file had 1 error')
   })
 
   it('catches mismatched header counts', async () => {
-    mockReadLinesAndType(`${mockDir}/header_count_mismatch.tsv`)
-    const { errors, summary } = await ValidateFile.validateFile({}, 'metadata')
-
+    mockReadLinesAndType('header_count_mismatch.tsv')
+    const { errors, summary } = await validateFile({}, 'metadata')
     expect(errors).toHaveLength(1)
     expect(summary).toBe('Your metadata file had 1 error')
   })
 
   it('catches multiple header errors', async () => {
-    mockReadLinesAndType(`${mockDir}/error_headers_v2.0.0.tsv`)
-
-    const { errors, summary } = await ValidateFile.validateFile({}, 'metadata')
-
+    mockReadLinesAndType('error_headers_v2.0.0.tsv')
+    const { errors, summary } = await validateFile({}, 'metadata')
     expect(errors).toHaveLength(3)
     expect(summary).toBe('Your metadata file had 3 errors')
   })
@@ -97,10 +89,8 @@ describe('Client-side file validation', () => {
     //
     // eslint-disable-next-line max-len
     // https://github.com/broadinstitute/scp-ingest-pipeline/blob/af1c124993f4a3e953debd5a594124f1ac52eee7/tests/test_cluster.py#L9
-    mockReadLinesAndType(`${mockDir}/cluster_bad_no_coordinates.txt`)
-
-    const { errors } = await ValidateFile.validateFile({}, 'cluster')
-
+    mockReadLinesAndType('cluster_bad_no_coordinates.txt')
+    const { errors } = await validateFile({}, 'cluster')
     expect(errors).toHaveLength(0)
   })
 
@@ -109,11 +99,8 @@ describe('Client-side file validation', () => {
     //
     // eslint-disable-next-line max-len
     // Mirrors https://github.com/broadinstitute/scp-ingest-pipeline/blob/af1c124993f4a3e953debd5a594124f1ac52eee7/tests/test_cluster.py#L21
-    const mockPath = `${mockDir}/cluster_example.txt`
-    mockReadLinesAndType(mockPath)
-
-    const { errors } = await ValidateFile.validateFile({}, 'cluster')
-
+    mockReadLinesAndType('cluster_example.txt')
+    const { errors } = await validateFile({}, 'cluster')
     expect(errors).toHaveLength(0)
   })
 
@@ -122,11 +109,8 @@ describe('Client-side file validation', () => {
     //
     // eslint-disable-next-line max-len
     // https://github.com/broadinstitute/scp-ingest-pipeline/blob/af1c124993f4a3e953debd5a594124f1ac52eee7/tests/test_cell_metadata.py#L17
-    const mockPath = `${mockDir}/metadata_bad_has_coordinates.txt`
-    mockReadLinesAndType(mockPath)
-
-    const { errors } = await ValidateFile.validateFile({}, 'metadata')
-
+    mockReadLinesAndType('metadata_bad_has_coordinates.txt')
+    const { errors } = await validateFile({}, 'metadata')
     expect(errors).toHaveLength(0)
   })
 
@@ -135,10 +119,8 @@ describe('Client-side file validation', () => {
     //
     // eslint-disable-next-line max-len
     // https:// github.com/broadinstitute/scp-ingest-pipeline/blob/af1c124993f4a3e953debd5a594124f1ac52eee7/tests/test_cell_metadata.py#L31
-    mockReadLinesAndType(`${mockDir}/metadata_example.txt`)
-
-    const { errors } = await ValidateFile.validateFile({}, 'metadata')
-
+    mockReadLinesAndType('metadata_example.txt')
+    const { errors } = await validateFile({}, 'metadata')
     expect(errors).toHaveLength(0)
   })
 
