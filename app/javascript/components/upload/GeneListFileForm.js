fc3fed19462a833647751afa75cf414604c32ee1
Devon Bush <dbush@broadinstitute.org>
2021-09-30 09:24:48 -0400

    adding gene lists upload step

---
+++ app/javascript/components/upload/GeneListFileForm.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import FileUploadControl from './FileUploadControl'
+import { TextFormField, SavingOverlay, SaveDeleteButtons } from './form-components'
+
+/** renders a form for editing/uploading a miscellaneous file */
+export default function GeneListFileForm({
+  file,
+  updateFile,
+  saveFile,
+  deleteFile,
+  handleSaveResponse,
+  miscFileTypes
+}) {
+  return <div className="row top-margin" key={file._id}>
+    <div className="col-md-12">
+      <form id={`misc-file-form-${file._id}`}
+        className="form-terra"
+        onSubmit={e => e.preventDefault()}
+        acceptCharset="UTF-8">
+        <div className="row">
+          <div className="col-md-12">
+            <FileUploadControl
+              handleSaveResponse={handleSaveResponse}
+              file={file}
+              updateFile={updateFile}/>
+          </div>
+        </div>
+        <div className="form-group">
+          <TextFormField label="Description" fieldName="description" file={file} updateFile={updateFile}/>
+        </div>
+        <SaveDeleteButtons file={file} updateFile={updateFile} saveFile={saveFile} deleteFile={deleteFile}/>
+      </form>
+
+      <SavingOverlay file={file} updateFile={updateFile}/>
+    </div>
+  </div>
+}
