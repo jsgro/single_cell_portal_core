8d0581d8da0cca928b8694276fdbe5928f6bb81a
Eric Weitz <eweitz@broadinstitute.org>
2021-12-03 15:54:26 -0500

    Replace deprecated postcss-cssnext with Webpacker 5 equivalents
    Prompted by CVE-2021-29060

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -12,5 +12,31 @@ if (environment.config.optimization) {
   environment.config.optimization.minimizer.find(m => m.constructor.name === 'TerserPlugin').options.parallel = 4
 }
 
+/**
+ * Fixes following error seen upon running `bin/webpack-dev-server`
+ * ValidationError: Invalid options object. PostCSS Loader has been initialized using an options object that does not match the API schema.
+ *
+ * From https://github.com/rails/webpacker/issues/2784#issuecomment-737003955
+ * by way of https://candland.net/2021/upgrading-rails-webpacker-tailwindcss/
+ */
+function hotfixPostcssLoaderConfig(subloader) {
+  const subloaderName = subloader.loader
+  if (subloaderName === 'postcss-loader') {
+    if (subloader.options.postcssOptions) {
+      console.log(
+        '\x1b[31m%s\x1b[0m',
+        'Remove postcssOptions workaround in config/webpack/environment.js'
+      )
+    } else {
+      subloader.options.postcssOptions = subloader.options.config
+      delete subloader.options.config
+    }
+  }
+}
+
+environment.loaders.keys().forEach(loaderName => {
+  const loader = environment.loaders.get(loaderName)
+  loader.use.forEach(hotfixPostcssLoaderConfig)
+})
 
 module.exports = environment
