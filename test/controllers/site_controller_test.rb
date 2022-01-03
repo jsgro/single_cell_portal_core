add13845a1a9acd8dc3bafc16232a804e5ce903d
bistline <bistline@broadinstitute.org>
2022-01-03 16:32:20 -0500

    fixing site_controller_test.rb for idempotency/seeds/logging

--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -1,14 +1,38 @@
-require "integration_test_helper"
+require 'integration_test_helper'
+require 'test_helper'
 
 class SiteControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
+  include Minitest::Hooks
+  include ::SelfCleaningSuite
+  include ::TestInstrumentor
+
+  before(:all) do
+    @user = FactoryBot.create(:admin_user, test_array: @@users_to_clean)
+    @sharing_user = FactoryBot.create(:user, test_array: @@users_to_clean)
+    @study = FactoryBot.create(:study,
+                               name_prefix: 'Site Controller Study',
+                               public: true,
+                               user: @user,
+                               test_array: @@studies_to_clean)
+    detail = @study.build_study_detail
+    detail.full_description = '<p>This is the description.</p>'
+    detail.save!
+
+    # manually create study files to facilitate pushing to Terra
+    filenames = %w[cluster_example.txt metadata_example.txt]
+    filenames.each do |filename|
+      file_type = filename =~ /cluster/ ? 'Cluster' : 'Metadata'
+      File.open(Rails.root.join('test', 'test_data', filename)) do |upload|
+        study_file = StudyFile.create!(name: filename, upload: upload, study: @study, file_type: file_type)
+        @study.send_to_firecloud(study_file)
+      end
+    end
+  end
 
   def setup
-    @test_user = User.find_by(email: 'testing.user@gmail.com')
-    auth_as_user(@test_user)
-    sign_in @test_user
-    @random_seed = File.open(Rails.root.join('.random_seed')).read.strip
-    @study = Study.find_by(name: "Testing Study #{@random_seed}")
+    auth_as_user(@user)
+    sign_in @user
   end
 
   def teardown
@@ -18,39 +42,32 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should redirect to home page from bare domain' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
     get '/'
     assert_response 302, "Did not receive correct HTTP status code, expected 302 but found #{status}"
     assert_redirected_to site_path, "Did not provide correct redirect, should have gone to #{site_path} but found #{path}"
     follow_redirect!
     assert_equal(site_path, path, "Redirect did not successfully complete, #{site_path} != #{path}")
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 
   test 'should redirect to correct study name url' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
     correct_study_url = view_study_path(accession: @study.accession, study_name: @study.url_safe_name)
     incorrect_study_url = view_study_path(accession: @study.accession, study_name: "bogus_name")
     get incorrect_study_url
     assert_response 302, 'Did not redirect to correct url'
-    assert_redirected_to correct_study_url,  "Url did not redirected successfully"
+    assert_redirected_to correct_study_url, 'Url did not redirected successfully'
     follow_redirect!
     assert_equal(correct_study_url, path, "Url is #{path}. Expected #{correct_study_url}")
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 
-  test "should create and delete deployment notification banner" do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
+  test 'should create and delete deployment notification banner' do
     deployment_notification_params = {
-        deployment_notification: {
-            deployment_time: Time.zone.now,
-            message: "Testing deployment notification banner"
-        }
+      deployment_notification: {
+        deployment_time: Time.zone.now,
+        message: 'Testing deployment notification banner'
+      }
     }
 
-    post create_deployment_notification_path, params: deployment_notification_params, xhr:true
+    post create_deployment_notification_path, params: deployment_notification_params, xhr: true
     get site_path
     assert_select '.notification-banner', 1,"Notification banner did not render to page"
     delete delete_deployment_notification_path
@@ -58,13 +75,9 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
     assert_response 200, 'Did not redirect successfully after banner was deleted'
     # Ensure page does not contain notification banner
     assert_select ".notification-banner", false, "Notification banner was not deleted and still is present on page."
-
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 
   test 'should control access to private studies' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
     get view_study_path(accession: @study.accession, study_name: @study.url_safe_name)
     assert_response :success
 
@@ -74,13 +87,12 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     # negative tests
-    sign_out(@test_user)
+    sign_out(@user)
     get view_study_path(accession: @study.accession, study_name: @study.url_safe_name)
     assert_response 302
     follow_redirect!
     assert_equal new_user_session_path, path
 
-    @sharing_user = User.find_by(email: 'sharing.user@gmail.com')
     auth_as_user(@sharing_user)
     sign_in @sharing_user
 
@@ -88,46 +100,38 @@ class SiteControllerTest < ActionDispatch::IntegrationTest
     assert_response 302
     follow_redirect!
     assert_equal site_path, path
-
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 
   test 'should control access to files in private studies' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
     file = @study.study_files.sample
     get download_file_path(accession: @study.accession, study_name: @study.url_safe_name, filename: file.upload_file_name)
     assert_response 302
     # since this is an external redirect, we cannot call follow_redirect! but instead have to get the location header
     signed_url = response.headers['Location']
-    assert signed_url.include?(file.upload_file_name), "Redirect url does not point at requested file"
+    assert signed_url.include?(file.upload_file_name), 'Redirect url does not point at requested file'
 
     # set to private, validate study owner/admin can still access
-    # note that download_file_path and download_private_file_path both resolve to the same method and enforce the same restrictions
-    # both paths are preserved for legacy redirects from published papers
+    # note that download_file_path and download_private_file_path both resolve to the same method and enforce the same
+    # restrictions; both paths are preserved for legacy redirects from published papers
     @study.update(public: false)
     get download_private_file_path(accession: @study.accession, study_name: @study.url_safe_name, filename: file.upload_file_name)
     assert_response 302
     signed_url = response.headers['Location']
-    assert signed_url.include?(file.upload_file_name), "Redirect url does not point at requested file"
+    assert signed_url.include?(file.upload_file_name), 'Redirect url does not point at requested file'
 
     # negative tests
-    sign_out(@test_user)
+    sign_out(@user)
     get download_private_file_path(accession: @study.accession, study_name: @study.url_safe_name, filename: file.upload_file_name)
     assert_response 302
     follow_redirect!
-    assert_equal new_user_session_path, path, "Did not redirect to sign in page"
+    assert_equal new_user_session_path, path, 'Did not redirect to sign in page'
 
-    @sharing_user = User.find_by(email: 'sharing.user@gmail.com')
     auth_as_user(@sharing_user)
     sign_in @sharing_user
 
     get download_private_file_path(accession: @study.accession, study_name: @study.url_safe_name, filename: file.upload_file_name)
     assert_response 302
     follow_redirect!
-    assert_equal site_path, path, "Did not redirect to home page"
-
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
+    assert_equal site_path, path, 'Did not redirect to home page'
   end
-
 end
