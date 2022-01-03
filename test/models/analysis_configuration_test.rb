fe6e172f5244201a753a97c8053c3653d75a5c55
bistline <bistline@broadinstitute.org>
2022-01-03 13:38:25 -0500

    fixing api_site_controller_test.rb and other impacted tests re: seeds

--- test/models/analysis_configuration_test.rb
+++ test/models/analysis_configuration_test.rb
@@ -1,37 +1,38 @@
 require "test_helper"
 
 class AnalysisConfigurationTest < ActiveSupport::TestCase
-  def setup
-    @analysis_configuration = AnalysisConfiguration.first
+  include Minitest::Hooks
+  include ::SelfCleaningSuite
+  include ::TestInstrumentor
+
+  before(:all) do
+    @user = FactoryBot.create(:admin_user, test_array: @@users_to_clean)
+    @analysis_configuration = AnalysisConfiguration.create(
+      namespace: 'single-cell-portal', name: 'split-cluster', snapshot: 1, user: @user,
+      configuration_namespace: 'single-cell-portal', configuration_name: 'split-cluster', configuration_snapshot: 2,
+      description: 'This is a test description.'
+    )
+  end
+
+  after(:all) do
+    AnalysisConfiguration.destroy_all
   end
 
   test 'load required parameters from methods repo' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
     remote_config = @analysis_configuration.methods_repo_settings
     local_config = @analysis_configuration.configuration_settings
     assert local_config === remote_config, "local configs does not match remote configs; diff: #{compare_hashes(remote_config, local_config)}"
-
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 
   test 'can extract required inputs from configuration' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
     remote_config = @analysis_configuration.methods_repo_settings
     inputs = @analysis_configuration.required_inputs
     assert remote_config['inputs'] == inputs, "required inputs do not match; diff: #{compare_hashes(remote_config['inputs'], inputs)}"
-
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 
   test 'can extract required outputs from configuration' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
     remote_config = @analysis_configuration.methods_repo_settings
     outputs = @analysis_configuration.required_outputs
     assert remote_config['outputs'] == outputs, "required inputs do not match; diff: #{compare_hashes(remote_config['outputs'], outputs)}"
-
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 end
--- test/models/analysis_parameter_test.rb
+++ test/models/analysis_parameter_test.rb
@@ -1,18 +1,27 @@
 require "test_helper"
 
 class AnalysisParameterTest < ActiveSupport::TestCase
-  def setup
-    @analysis_configuration = AnalysisConfiguration.first
+  include Minitest::Hooks
+  include ::SelfCleaningSuite
+  include ::TestInstrumentor
+
+  before(:all) do
+    @user = FactoryBot.create(:admin_user, test_array: @@users_to_clean)
+    @analysis_configuration = AnalysisConfiguration.create(
+      namespace: 'single-cell-portal', name: 'split-cluster', snapshot: 1, user: @user,
+      configuration_namespace: 'single-cell-portal', configuration_name: 'split-cluster', configuration_snapshot: 2,
+      description: 'This is a test description.'
+    )
+  end
+
+  after(:all) do
+    AnalysisConfiguration.destroy_all
   end
 
   test 'should validate output file types' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
     output_param = @analysis_configuration.analysis_parameters.outputs.first
     assert output_param.is_output_file?, "Should have returned true for is_output_file?: #{output_param.is_output_file?}"
     output_param.output_file_type = "Not a file"
     assert !output_param.valid?, "Should not have validated with an invalid file type"
-
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 end
