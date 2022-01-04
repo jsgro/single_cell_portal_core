c6c1283c10b32ae37ed6628b6d3fdc1e5e11ae9a
bistline <bistline@broadinstitute.org>
2022-01-04 14:06:48 -0500

    porting cell_metadatum_test.rb to FactoryBot

--- test/models/cell_metadatum_test.rb
+++ test/models/cell_metadatum_test.rb
@@ -1,10 +1,20 @@
-require "test_helper"
+require 'test_helper'
 
 class CellMetadatumTest < ActiveSupport::TestCase
+  include Minitest::Hooks
+  include TestInstrumentor
+  include SelfCleaningSuite
+
+  before(:all) do
+    @user = FactoryBot.create(:user, test_array: @@users_to_clean)
+    @study = FactoryBot.create(:detached_study,
+                               name_prefix: 'CellMetadatum Test',
+                               user: @user,
+                               test_array: @@studies_to_clean)
+    @metadata_file = FactoryBot.create(:metadata_file, name: 'metadata.txt', study: @study)
+  end
 
   test 'should not visualize unique group annotations over 100' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
     # setup
     annotation_values = []
     300.times { annotation_values << SecureRandom.uuid }
@@ -19,27 +29,22 @@ class CellMetadatumTest < ActiveSupport::TestCase
     @cell_metadatum.values = []
     can_visualize = @cell_metadatum.can_visualize?
     assert can_visualize, "Should be able to visualize numeric annotations at any level: #{can_visualize}"
-
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 
   test 'should not visualize ontology id based annotations' do
-    puts "#{File.basename(__FILE__)}: #{self.method_name}"
-
     # setup
-    study = Study.first
-    metadata_file = study.metadata_file
-    @disease = CellMetadatum.create(study_id: study.id, study_file_id: metadata_file.id, name: 'disease',
+    @disease = CellMetadatum.create(study_id: @study.id, study_file_id: @metadata_file.id, name: 'disease',
                                     annotation_type: 'group', values: %w(MONDO_0000001))
-    @disease_labels = CellMetadatum.create(study_id: study.id, study_file_id: metadata_file.id, name: 'disease__ontology_label',
+    @disease_labels = CellMetadatum.create(study_id: @study.id, study_file_id: @metadata_file.id, name: 'disease__ontology_label',
                                            annotation_type: 'group', values: ['disease or disorder'])
 
     # ensure id-based annotations do not visualize
     assert @disease.is_ontology_ids?, "Did not correctly identify #{@disease.name} annotation as ontology ID based"
-    refute @disease_labels.is_ontology_ids?, "Incorrectly labelled #{@disease_labels.name} annotation as ontology ID based"
-    refute @disease.can_visualize?,
+    assert_not @disease_labels.is_ontology_ids?,
+               "Incorrectly labelled #{@disease_labels.name} annotation as ontology ID based"
+    assert_not @disease.can_visualize?,
            "Should not be able to view #{@disease.name} annotation: values: #{@disease.values.size}, is_ontology_ids?: #{@disease.is_ontology_ids?}"
-    refute @disease_labels.can_visualize?,
+    assert_not @disease_labels.can_visualize?,
            "Should not be able to view #{@disease_labels.name} annotation: values: #{@disease_labels.values.size}, is_ontology_ids?: #{@disease_labels.is_ontology_ids?}"
 
     # update disease__ontology_label to have more than one value
@@ -50,7 +55,5 @@ class CellMetadatumTest < ActiveSupport::TestCase
     # clean up
     @disease.destroy
     @disease_labels.destroy
-
-    puts "#{File.basename(__FILE__)}: #{self.method_name} successful!"
   end
 end
