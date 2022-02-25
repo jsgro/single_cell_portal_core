6834b438d290d27b554d7d0655936eb1452e4055
Devon Bush <dbush@broadinstitute.org>
2022-02-24 20:50:08 -0500

    emptying old application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,1 @@
-import $ from 'jquery'
-
-window.$ = $
-window.jQuery = $
-
-window.SCP.componentsToRender = []
-window.SCP.renderComponentFromRails = function(targetId, componentName, args) {
-  if (window.SCP.renderComponent) {
-    window.SCP.renderComponent(targetId, componentName, args)
-  } else {
-    window.SCP.componentsToRender.push({
-      targetId, componentName, args
-    })
-  }
-}
+// Nothing to see here
