517ec83971d1eec7a2c0a5e78653b94e76d4e546
Devon Bush <dbush@broadinstitute.org>
2022-02-28 23:19:07 -0500

    removing debug mode

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  'mode': 'development', // < this is important
-  'build': {
-    minify: false,
-    sourcemap: true // < this allows the browser to point you to the correct file
-  },
   'plugins': [
     // inject plugin needs to be first
     rubyPlugin(),
