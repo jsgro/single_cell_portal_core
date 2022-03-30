eefc9d352c7bd4ed74d8b9cce9054d9943e2c638
ehanna4 <54322292+ehanna4@users.noreply.github.com>
2022-03-30 13:36:36 -0400

    Update app/lib/bulk_download_service.rb
    Co-authored-by: Eric Weitz <eweitz@broadinstitute.org>

--- app/lib/bulk_download_service.rb
+++ app/lib/bulk_download_service.rb
@@ -150,7 +150,7 @@ class BulkDownloadService
     if bytes_requested > bytes_allowed
       raise RuntimeError.new "Total file size exceeds user download quota: #{bytes_requested} bytes requested, #{bytes_allowed} bytes allowed"
     else
-      Rails.logger.info "Adding '#{bytes_requested}' bytes to user: '#{user.id}' download quota for bulk download"
+      Rails.logger.info "Adding #{bytes_requested} bytes to user: #{user.id} download quota for bulk download"
       user.daily_download_quota += bytes_requested
       user.save
     end
