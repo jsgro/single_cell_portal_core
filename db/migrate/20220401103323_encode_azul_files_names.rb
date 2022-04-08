2ef2a518a1ac92b2e8dc78985860cf274ac5bf89
ehanna4 <54322292+ehanna4@users.noreply.github.com>
2022-04-08 09:44:52 -0400

    Update db/migrate/20220401103323_encode_azul_files_names.rb
    Co-authored-by: Eric Weitz <eweitz@broadinstitute.org>

--- db/migrate/20220401103323_encode_azul_files_names.rb
+++ db/migrate/20220401103323_encode_azul_files_names.rb
@@ -10,7 +10,7 @@ class EncodeAzulFilesNames < Mongoid::Migration
     def self.down
     download_reqs = DownloadRequest.where(:azul_files.ne => nil)
     download_reqs.each do |req|
-        decoded_azul_files = DownloadRequest.transform_files( req.azul_files, :decode)
+        decoded_azul_files = DownloadRequest.transform_files(req.azul_files, :decode)
         req.update(azul_files: decoded_azul_files)
       end
     end
