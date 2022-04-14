3332dcb1fcca2afef7b51eac36498c295c853ba0
Emily Hanna <ehanna@broadinstitute.org>
2022-04-14 15:43:38 -0400

    Update 20220401103323_encode_azul_files_names.rb

--- db/migrate/20220401103323_encode_azul_files_names.rb
+++ db/migrate/20220401103323_encode_azul_files_names.rb
@@ -2,7 +2,7 @@ class EncodeAzulFilesNames < Mongoid::Migration
     def self.up
       download_reqs = DownloadRequest.where(:azul_files.ne => nil)
       download_reqs.each do |req|
-        encoded_azul_files = DownloadRequest.transform_files(req.azul_files, :encode)
+        encoded_azul_files = ActiveSupport::JSON.encode(req.azul_files)
         req.update(azul_files: encoded_azul_files)
       end
     end
@@ -10,8 +10,8 @@ class EncodeAzulFilesNames < Mongoid::Migration
     def self.down
     download_reqs = DownloadRequest.where(:azul_files.ne => nil)
     download_reqs.each do |req|
-      azul_files_as_hash = DownloadRequest.transform_files(req.azul_files, :decode)
-        req.update(azul_files: azul_files_as_hash)
+      azul_files_as_hash = ActiveSupport::JSON.decode(req.azul_files.gsub('=>', ':'))
+      req.update(azul_files: azul_files_as_hash)
       end
     end
   end
