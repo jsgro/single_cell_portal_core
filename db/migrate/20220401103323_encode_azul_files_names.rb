e0e9de2795201b21b4f0c91a73ea6fe265fa1115
Emily Hanna <ehanna@broadinstitute.org>
2022-04-14 15:59:02 -0400

    Fix indents

--- db/migrate/20220401103323_encode_azul_files_names.rb
+++ db/migrate/20220401103323_encode_azul_files_names.rb
@@ -8,10 +8,10 @@ class EncodeAzulFilesNames < Mongoid::Migration
     end
   
     def self.down
-    download_reqs = DownloadRequest.where(:azul_files.ne => nil)
-    download_reqs.each do |req|
-      azul_files_as_hash = ActiveSupport::JSON.decode(req.azul_files.gsub('=>', ':'))
-      req.update(azul_files: azul_files_as_hash)
+      download_reqs = DownloadRequest.where(:azul_files.ne => nil)
+      download_reqs.each do |req|
+        azul_files_as_hash = ActiveSupport::JSON.decode(req.azul_files.gsub('=>', ':'))
+        req.update(azul_files: azul_files_as_hash)
       end
     end
   end
