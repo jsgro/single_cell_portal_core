cc75314514c45ff5a0b63bdb3cfa27eb87e91fd9
Emily Hanna <ehanna@broadinstitute.org>
2022-04-28 11:10:11 -0400

    Better error for corrupted file

--- app/javascript/lib/validation/validate-file-content.js
+++ app/javascript/lib/validation/validate-file-content.js
@@ -425,6 +425,9 @@ async function parseFile(file, fileType, fileOptions={}, sizeProps={}) {
     // get any unhandled or deliberate short-circuits
     if (error instanceof ParseException) {
       parseResult.issues.push(['error', error.key, error.message])
+    } else if (error instanceof TypeError) {
+      const msg = 'File cannot be uploaded in it\'s current state. Please reach out for assistance'
+      parseResult.issues.push(['error', error.key, msg])
     } else {
       parseResult.issues.push(['error', 'parse:unhandled', error.message])
     }
