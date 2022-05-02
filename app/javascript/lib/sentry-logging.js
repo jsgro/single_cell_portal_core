d46c665442181aaaad2b20255894c9be6601f39e
Emily Hanna <ehanna@broadinstitute.org>
2022-05-02 16:21:27 -0400

    simpler local dev

--- app/javascript/lib/sentry-logging.js
+++ app/javascript/lib/sentry-logging.js
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/react'
 import getSCPContext from '~/providers/SCPContextProvider'
 
 const env = getSCPContext().environment
-setUptoLog()
+const doLog = setUptoLog()
 
 /**
  * Log an exception to Sentry for bad response JS fetch executions
@@ -16,9 +16,6 @@ setUptoLog()
  * @param {String} titleInfo - extra info for the title of the Sentry event
  */
 export function logJSFetchExceptionToSentry(response, titleInfo = {}) {
-  if (['development', 'test'].includes(env)) {
-    return
-  }
   // add details from the response to the 'response info' object that will be logged in Sentry
   Sentry.setContext('response info', {
     status: response.status,
@@ -26,7 +23,7 @@ export function logJSFetchExceptionToSentry(response, titleInfo = {}) {
     url: response.url
   })
 
-  Sentry.captureException(new Error(`${response.status}: ${titleInfo}`))
+  doLog && Sentry.captureException(new Error(`${response.status}: ${titleInfo}`))
 }
 
 /**
@@ -40,13 +37,19 @@ export function logJSFetchErrorToSentry(error, titleInfo = {}) {
   if (['development', 'test'].includes(env)) {
     return
   }
-  Sentry.captureException(new Error(`${error}: ${titleInfo}`))
+  doLog && Sentry.captureException(new Error(`${error}: ${titleInfo}`))
 }
 
 /**
  * Set up the correct scope and set the logger tag to the appropriate name
  */
 function setUptoLog() {
+// do not log for development or test environments
+// to test locally comment out lines 49-51
+  if (['development', 'test'].includes(env)) {
+    return false
+  }
+
   // set the scope to the current environment
   Sentry.configureScope(scope =>
     scope.addEventProcessor(
@@ -62,4 +65,5 @@ function setUptoLog() {
 
   // JS errors will be logged as 'app-frontend' in Sentry
   Sentry.setTag('logger', 'app-frontend')
+  return true
 }
