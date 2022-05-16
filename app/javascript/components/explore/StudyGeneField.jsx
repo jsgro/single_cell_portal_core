3188dc358b478c7b142a6010a75d7c1555a59f92
Emily Hanna <ehanna@broadinstitute.org>
2022-05-16 16:46:17 -0400

    Update StudyGeneField.jsx

--- app/javascript/components/explore/StudyGeneField.jsx
+++ app/javascript/components/explore/StudyGeneField.jsx
@@ -127,7 +127,7 @@ export default function StudyGeneField({ genes, searchGenes, allGenes, speciesLi
     // react-select doesn't expose the actual click events, so we deduce the kind
     // of operation based on whether it lengthened or shortened the list
     const newValue = value ? value : []
-    setBadGeneArray(newValue === [] ? []: badGeneArray)
+    setBadGeneArray([])
     logGeneArrayChange(newValue)
     setGeneArray(newValue)
   }
@@ -137,6 +137,7 @@ export default function StudyGeneField({ genes, searchGenes, allGenes, speciesLi
       // the genes have been updated elsewhere -- resync
       setGeneArray(getOptionsFromGenes(genes))
       setInputText('')
+      setBadGeneArray([])
     }
   }, [genes.join(',')])
 
