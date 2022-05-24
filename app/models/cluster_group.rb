8feff55f28b386e39c22a381bd88a5051a1d7287
Devon Bush <dbush@broadinstitute.org>
2022-05-23 22:26:45 -0400

    fixing tests

--- app/models/cluster_group.rb
+++ app/models/cluster_group.rb
@@ -140,7 +140,7 @@ class ClusterGroup
     annot = annotation.with_indifferent_access
     if annot[:type] == 'group'
       CellMetadatum::GROUP_VIZ_THRESHOLD === annot[:values].count ||
-        study.override_viz_limit_annotations.include?(annot[:name])
+        self.study.override_viz_limit_annotations.include?(annot[:name])
     else
       true
     end
