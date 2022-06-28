c5a56ae5fe51c6e50db9a27bc5afaabc977133ce
bistline <bistline@broadinstitute.org>
2022-06-28 11:41:17 -0400

    enabling full CI run, adding image cleanup in GCR

--- bin/build_image.sh
+++ bin/build_image.sh
@@ -38,7 +38,15 @@ function main {
 
   echo "*** BUILDING IMAGE REF $IMAGE_NAME:$VERSION_TAG ***"
   docker build -t $IMAGE_NAME:$VERSION_TAG . || exit_with_error_message "could not build docker image"
-  echo "*** BUILD COMPLETE, PUSHING $IMAGE_NAME:$VERSION_TAG ***"
+  echo "*** BUILD COMPLETE ***"
+  # check if we need to remove an existing image so that we don't have orphaned images hanging around
+  EXISTING_DIGEST=$(gcloud container images list-tags $IMAGE_NAME --filter="tags:$VERSION_TAG" --format="csv(digest)[no-heading]")
+  if [[ -n "$EXISTING_DIGEST" ]]; then
+    echo "*** REMOVING EXISTING IMAGE DIGEST $EXISTING_DIGEST FOR $IMAGE_NAME:$VERSION_TAG ***"
+    gcloud container images delete "$IMAGE_NAME@$EXISTING_DIGEST"
+    echo "*** IMAGE REMOVAL COMPLETE ***"
+  fi
+  echo "*** PUSHING $IMAGE_NAME:$VERSION_TAG ***"
   docker push $IMAGE_NAME:$VERSION_TAG || exit_with_error_message "could not push docker image $IMAGE_NAME:$VERSION_TAG"
   echo "*** PUSH COMPLETE ***"
 }
