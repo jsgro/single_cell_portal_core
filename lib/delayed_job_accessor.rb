8a55ad8e74c94393ebeda9880a4c74bd5a9184eb
bistline <bistline@broadinstitute.org>
2022-08-08 17:22:36 -0400

    updating safe class list

--- lib/delayed_job_accessor.rb
+++ lib/delayed_job_accessor.rb
@@ -8,7 +8,7 @@ module DelayedJobAccessor
 
   # Classes allowed to dump handlers from using YAML.safe_load
   # This covers all recursive data structures inside IngestJob and UploadCleanupJob
-  SAFE_CLASS_LOADERS = [IngestJob, UploadCleanupJob, Symbol, BSON::ObjectId, Time].freeze
+  SAFE_CLASS_LOADERS = [IngestJob, UploadCleanupJob, Symbol, BSON::ObjectId, BSON::Document, Time].freeze
 
   # find a Delayed::Job instance of a particular class, and refine by an associated object
   #
