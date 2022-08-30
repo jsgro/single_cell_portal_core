0985c58bd96bcc619a497ac3e4b4ae97cfebbdb5
bistline <bistline@broadinstitute.org>
2022-08-30 09:47:00 -0400

    fixing whitespace

--- set_user_permissions.bash
+++ set_user_permissions.bash
@@ -3,7 +3,7 @@ if [[ $PASSENGER_APP_ENV = "production" ]] || [[ $PASSENGER_APP_ENV = "staging" 
 then
 	set -e # fail on any error
 
-  echo '*** SETTING UID AND GID TO MATCH HOST VOLUMES ***'
+	echo '*** SETTING UID AND GID TO MATCH HOST VOLUMES ***'
 	TARGET_UID=$(stat -c "%u" /home/app/webapp)
 	echo '-- Setting app user to use uid '$TARGET_UID
 	usermod -o -u $TARGET_UID app || true
