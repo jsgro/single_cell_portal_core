335f15902604f77912a04a5c93965e2ff71786ad
ehanna4 <ehanna@broadinstitute.org>
2022-10-13 10:24:02 -0400

    PR feedback 2

--- db/migrate/20221006132730_update_misc_file_type_for_anndata_and_seurat.rb
+++ db/migrate/20221006132730_update_misc_file_type_for_anndata_and_seurat.rb
@@ -4,7 +4,7 @@
       StudyFile.where(:file_type.in => ['Other', 'Documentation', 'Analysis Output'], upload_file_name: /\.(h5ad|h5)$/).update_all(file_type: 'AnnData')
       
       # For Seurat
-      StudyFile.where(:file_type.in => ['Documentation', 'Other', 'Analysis Output'], upload_file_name: /\.(rds|Rdata)$/).update_all(file_type: 'Seurat')
+      StudyFile.where(:file_type.in => ['Documentation', 'Other', 'Analysis Output'], upload_file_name: /\.(rda|Rda|rds|Rds|Rdata$/).update_all(file_type: 'Seurat')
 
     end
   
