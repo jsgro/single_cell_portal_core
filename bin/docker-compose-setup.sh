1bbfdfd0209e3642ab39a033f087115672f034a7
bistline <bistline@broadinstitute.org>
2022-10-17 16:52:45 -0400

    use long form for readability

--- bin/docker-compose-setup.sh
+++ bin/docker-compose-setup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 echo "### SETTING UP ENVIRONMENT ###"
-./rails_local_setup.rb -d
+./rails_local_setup.rb --docker-paths
 source config/secrets/.source_env.bash
 echo "### STARTING SERVICES ###"
 docker-compose -f docker-compose-dev.yaml up
