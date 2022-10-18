08a45af2c5b1468b557bcfc204f7d3ab1f23c91a
bistline <bistline@broadinstitute.org>
2022-10-18 11:01:47 -0400

    adding entrypoint scripts, refining startup/cleanup procs

--- bin/docker-compose-cleanup.sh
+++ bin/docker-compose-cleanup.sh
@@ -2,9 +2,6 @@
 echo "### REMOVING CONTAINERS/VOLUMES ###"
 docker-compose -f docker-compose-dev.yaml down
 docker volume prune --force
-PID_PATH=tmp/pids/server.pid
-if [[ -f "$PID_PATH" ]]; then
-  rm $PID_PATH
-fi
+rm tmp/pids/*.pid
 echo "### REVERTING ENVIRONMENT ###"
 ./rails_local_setup.rb
