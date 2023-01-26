9039ecd9690c4dc4c4d677e8fc2a53896a6f3176
ehanna4 <ehanna@broadinstitute.org>
2023-01-26 12:54:16 -0500

    Revert "Merge branch 'jb-rails-6.1.7.1' into eh-update-ui-forms"
    This reverts commit bebe149ed05c310329c215d54e7980821c68022c, reversing
    changes made to 945e2029dce5f2cc915828c4606d5b23336e5ff9.

--- bin/docker-compose-cleanup.sh
+++ bin/docker-compose-cleanup.sh
@@ -4,8 +4,6 @@
 # stop/remove all containers/volumes created by docker-compose and revert environment config files
 # More context: https://github.com/broadinstitute/single_cell_portal_core#hybrid-docker-local-development
 
-# suppress '"GCR_IMAGE" variable is not set' warning, actual image/tag are irrelevant here
-export GCR_IMAGE="gcr.io/broad-singlecellportal-staging/single-cell-portal:development"
 echo "### REMOVING CONTAINERS/VOLUMES ###"
 # set VITE_FRONTEND_SERVICE_WORKER_CACHE to silence warnings from docker-compose
 VITE_FRONTEND_SERVICE_WORKER_CACHE="$VITE_FRONTEND_SERVICE_WORKER_CACHE" \
--- bin/docker-compose-setup.sh
+++ bin/docker-compose-setup.sh
@@ -9,15 +9,13 @@ cat <<EOF
 $0 [OPTION]
 -d   run docker-compose in detached mode (default is attatched to terminal STDOUT)
 -c   enable VITE_FRONTEND_SERVICE_WORKER_CACHE (default is disabled)
--i IMAGE_TAG   override default GCR image tag of development
 -h   print this text
 EOF
 )
 
 DETACHED=""
 VITE_FRONTEND_SERVICE_WORKER_CACHE="false"
-IMAGE_TAG="development"
-while getopts "dchi:" OPTION; do
+while getopts "dch" OPTION; do
 case $OPTION in
   d)
     echo "### SETTING DETACHED ###"
@@ -29,35 +27,21 @@ case $OPTION in
     VITE_FRONTEND_SERVICE_WORKER_CACHE="true"
     ;;
   h)
-    echo "$usage"
-    exit 0
-    ;;
-  i)
-    echo "### SETTING GCR IMAGE TAG TO $OPTARG ###"
-    IMAGE_TAG="$OPTARG"
-    ;;
+  	echo "$usage"
+  	exit 0
+  	;;
   *)
-    echo "unrecognized option"
-    echo "$usage"
-    exit 1
-    ;;
+  	echo "unrecognized option"
+  	echo "$usage"
+  	exit 1
+  	;;
   esac
 done
-export GCR_IMAGE="gcr.io/broad-singlecellportal-staging/single-cell-portal:$IMAGE_TAG"
+
 echo "### SETTING UP ENVIRONMENT ###"
 ./rails_local_setup.rb --docker-paths
 source config/secrets/.source_env.bash
 rm tmp/pids/*.pid
-# determine if there are upstream changes that would require a rebuild of the Docker image
-LOCAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-CHANGED=$(git diff "$LOCAL_BRANCH" development --name-only -- Dockerfile)
-if [[ "$CHANGED" = "Dockerfile" ]]; then
-  echo "### DOCKERFILE CHANGES DETECTED, BUILDING $GCR_IMAGE LOCALLY ###"
-  docker build -t "$GCR_IMAGE" .
-else
-  echo "### PULLING UPDATED IMAGE FOR $GCR_IMAGE ###"
-  docker pull "$GCR_IMAGE"
-fi
 echo "### STARTING SERVICES ###"
 VITE_FRONTEND_SERVICE_WORKER_CACHE="$VITE_FRONTEND_SERVICE_WORKER_CACHE" \
 docker-compose -f docker-compose-dev.yaml up $DETACHED
