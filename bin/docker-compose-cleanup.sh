1ea7362443667b3601a14ba1cbb9acf09054f86a
bistline <bistline@broadinstitute.org>
2023-03-02 16:23:47 -0500

    fixing comment typo

--- bin/docker-compose-cleanup.sh
+++ bin/docker-compose-cleanup.sh
@@ -4,7 +4,7 @@
 # stop/remove all containers/volumes created by docker-compose and revert environment config files
 # More context: https://github.com/broadinstitute/single_cell_portal_core#hybrid-docker-local-development
 
-# suppress variable is not set' warning, actual values are irrelevant
+# suppress 'variable is not set' warning, actual values are irrelevant
 export GCR_IMAGE="gcr.io/broad-singlecellportal-staging/single-cell-portal:development"
 export PORTAL_RAM_GB="6gb"
 export VITE_RAM_GB="2gb"
