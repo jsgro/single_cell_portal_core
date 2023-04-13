d9404b5aa5fefebb4065629d3247752777a5ec25
bistline <bistline@broadinstitute.org>
2023-04-13 17:24:13 -0400

    finishing mock coverage for API tests

--- test/api/study_file_bundles_controller_test.rb
+++ test/api/study_file_bundles_controller_test.rb
@@ -2,12 +2,13 @@ require 'api_test_helper'
 require 'user_tokens_helper'
 require 'test_helper'
 require 'includes_helper'
+require 'detached_helper'
 
 class StudyFileBundlesControllerTest < ActionDispatch::IntegrationTest
 
   before(:all) do
     @user = FactoryBot.create(:api_user, test_array: @@users_to_clean)
-    @study = FactoryBot.create(:study,
+    @study = FactoryBot.create(:detached_study,
                                name_prefix: 'StudyFileBundle Study',
                                public: true,
                                user: @user,
@@ -32,46 +33,52 @@ class StudyFileBundlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get index' do
-    execute_http_request(:get, api_v1_study_study_file_bundles_path(@study))
-    assert_response :success
-    assert json.size >= 1, 'Did not find any study_file_bundles'
+    mock_not_detached @study, :find_by do
+      execute_http_request(:get, api_v1_study_study_file_bundles_path(@study))
+      assert_response :success
+      assert json.size >= 1, 'Did not find any study_file_bundles'
+    end
   end
 
   test 'should get study file bundle' do
-    execute_http_request(:get, api_v1_study_study_file_bundle_path(study_id: @study.id, id: @study_file_bundle.id))
-    assert_response :success
-    # check all attributes against database
-    @study_file_bundle.attributes.each do |attribute, value|
-      if attribute =~ /_id/
-        assert json[attribute] == JSON.parse(value.to_json), "Attribute mismatch: #{attribute} is incorrect, expected #{JSON.parse(value.to_json)} but found #{json[attribute.to_s]}"
-      elsif attribute =~ /_at/
-        # ignore timestamps as formatting & drift on milliseconds can cause comparison errors
-        next
-      else
-        assert json[attribute] == value, "Attribute mismatch: #{attribute} is incorrect, expected #{value} but found #{json[attribute.to_s]}"
+    mock_not_detached @study, :find_by do
+      execute_http_request(:get, api_v1_study_study_file_bundle_path(study_id: @study.id, id: @study_file_bundle.id))
+      assert_response :success
+      # check all attributes against database
+      @study_file_bundle.attributes.each do |attribute, value|
+        if attribute =~ /_id/
+          assert json[attribute] == JSON.parse(value.to_json), "Attribute mismatch: #{attribute} is incorrect, expected #{JSON.parse(value.to_json)} but found #{json[attribute.to_s]}"
+        elsif attribute =~ /_at/
+          # ignore timestamps as formatting & drift on milliseconds can cause comparison errors
+          next
+        else
+          assert json[attribute] == value, "Attribute mismatch: #{attribute} is incorrect, expected #{value} but found #{json[attribute.to_s]}"
+        end
       end
     end
   end
 
   # create & delete tested together to use new object to avoid delete running before create
   test 'should create then delete study file bundle' do
-    # create study file bundle
-    study_file_bundle_attributes = {
-      'study_file_bundle' => {
-        'original_file_list' => [
-          { 'name' => 'matrix.mtx', 'file_type' => 'MM Coordinate Matrix' },
-          { 'name' => 'genes.tsv', 'file_type' => '10X Genes File' },
-          { 'name' => 'barcodes.tsv', 'file_type' => '10X Barcodes File' }
-        ]
+    mock_not_detached @study, :find_by do
+      # create study file bundle
+      study_file_bundle_attributes = {
+        'study_file_bundle' => {
+          'original_file_list' => [
+            { 'name' => 'matrix.mtx', 'file_type' => 'MM Coordinate Matrix' },
+            { 'name' => 'genes.tsv', 'file_type' => '10X Genes File' },
+            { 'name' => 'barcodes.tsv', 'file_type' => '10X Barcodes File' }
+          ]
+        }
       }
-    }
-    execute_http_request(:post, api_v1_study_study_file_bundles_path(study_id: @study.id), request_payload: study_file_bundle_attributes)
-    assert_response :success
-    assert json['original_file_list'] == study_file_bundle_attributes['study_file_bundle']['original_file_list'],
-           "Did not set name correctly, expected #{study_file_bundle_attributes['study_file_bundle']['original_file_list']} but found #{json['original_file_list']}"
-    # delete study file bundle
-    study_file_bundle_id = json['_id']['$oid']
-    execute_http_request(:delete, api_v1_study_study_file_bundle_path(study_id: @study.id, id: study_file_bundle_id))
-    assert_response 204, "Did not successfully delete study file bundle, expected response of 204 but found #{@response.response_code}"
+      execute_http_request(:post, api_v1_study_study_file_bundles_path(study_id: @study.id), request_payload: study_file_bundle_attributes)
+      assert_response :success
+      assert json['original_file_list'] == study_file_bundle_attributes['study_file_bundle']['original_file_list'],
+             "Did not set name correctly, expected #{study_file_bundle_attributes['study_file_bundle']['original_file_list']} but found #{json['original_file_list']}"
+      # delete study file bundle
+      study_file_bundle_id = json['_id']['$oid']
+      execute_http_request(:delete, api_v1_study_study_file_bundle_path(study_id: @study.id, id: study_file_bundle_id))
+      assert_response 204, "Did not successfully delete study file bundle, expected response of 204 but found #{@response.response_code}"
+    end
   end
 end
--- test/api/study_shares_controller_test.rb
+++ test/api/study_shares_controller_test.rb
@@ -2,13 +2,14 @@ require 'api_test_helper'
 require 'user_tokens_helper'
 require 'test_helper'
 require 'includes_helper'
+require 'detached_helper'
 
 class StudySharesControllerTest < ActionDispatch::IntegrationTest
 
   before(:all) do
     @user = FactoryBot.create(:api_user, test_array: @@users_to_clean)
     @other_user = FactoryBot.create(:api_user, test_array: @@users_to_clean)
-    @study = FactoryBot.create(:study,
+    @study = FactoryBot.create(:detached_study,
                                name_prefix: 'StudyFileBundle Study',
                                public: true,
                                user: @user,
@@ -26,55 +27,71 @@ class StudySharesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get index' do
-    execute_http_request(:get, api_v1_study_study_shares_path(@study))
-    assert_response :success
-    assert json.size >= 1, 'Did not find any study_shares'
+    mock_not_detached @study, :find_by do
+      execute_http_request(:get, api_v1_study_study_shares_path(@study))
+      assert_response :success
+      assert json.size >= 1, 'Did not find any study_shares'
+    end
   end
 
   test 'should get study share' do
-    execute_http_request(:get, api_v1_study_study_share_path(study_id: @study.id, id: @study_share.id))
-    assert_response :success
-    # check all attributes against database
-    @study_share.attributes.each do |attribute, value|
-      if attribute =~ /_id/
-        assert json[attribute] == JSON.parse(value.to_json),
-               "Attribute mismatch: #{attribute} is incorrect, expected #{JSON.parse(value.to_json)} but found #{json[attribute.to_s]}"
-      elsif attribute =~ /_at/
-        # ignore timestamps as formatting & drift on milliseconds can cause comparison errors
-        next
-      else
-        assert json[attribute] == value,
-               "Attribute mismatch: #{attribute} is incorrect, expected #{value} but found #{json[attribute.to_s]}"
+    mock_not_detached @study, :find_by do
+      execute_http_request(:get, api_v1_study_study_share_path(study_id: @study.id, id: @study_share.id))
+      assert_response :success
+      # check all attributes against database
+      @study_share.attributes.each do |attribute, value|
+        case attribute
+        when /_id/
+          assert json[attribute] == JSON.parse(value.to_json),
+                 "Attribute mismatch: #{attribute} is incorrect, expected #{JSON.parse(value.to_json)} " \
+                      "but found #{json[attribute.to_s]}"
+        when /_at/
+          # ignore timestamps as formatting & drift on milliseconds can cause comparison errors
+          next
+        else
+          assert json[attribute] == value,
+                 "Attribute mismatch: #{attribute} is incorrect, expected #{value} " \
+                      "but found #{json[attribute.to_s]}"
+        end
       end
     end
   end
 
   # create, update & delete tested together to use new object to avoid delete/update running before create
   test 'should create then update then delete study share' do
-    # create study share
-    study_share_attributes = {
-      study_share: {
-        email: 'some.person@gmail.com',
-        permission: 'Reviewer'
+    mock_not_detached @study, :find_by do
+      # create study share
+      study_share_attributes = {
+        study_share: {
+          email: 'some.person@gmail.com',
+          permission: 'Reviewer'
+        }
       }
-    }
-    execute_http_request(:post, api_v1_study_study_shares_path(study_id: @study.id), request_payload: study_share_attributes)
-    assert_response :success
-    assert json['email'] == study_share_attributes[:study_share][:email],
-           "Did not set email correctly, expected #{study_share_attributes[:study_share][:email]} but found #{json['email']}"
-    # update study share
-    study_share_id = json['_id']['$oid']
-    update_attributes = {
-      study_share: {
-        deliver_emails: false
+      execute_http_request(
+        :post, api_v1_study_study_shares_path(study_id: @study.id), request_payload: study_share_attributes
+      )
+      assert_response :success
+      assert json['email'] == study_share_attributes[:study_share][:email],
+             "Did not set email correctly, expected #{study_share_attributes[:study_share][:email]} " \
+                  "but found #{json['email']}"
+      # update study share
+      study_share_id = json['_id']['$oid']
+      update_attributes = {
+        study_share: {
+          deliver_emails: false
+        }
       }
-    }
-    execute_http_request(:patch, api_v1_study_study_share_path(study_id: @study.id, id: study_share_id), request_payload: update_attributes)
-    assert_response :success
-    assert json['deliver_emails'] == update_attributes[:study_share][:deliver_emails],
-           "Did not set deliver_emails correctly, expected #{update_attributes[:study_share][:deliver_emails]} but found #{json['deliver_emails']}"
-    # delete study share
-    execute_http_request(:delete, api_v1_study_study_share_path(study_id: @study.id, id: study_share_id))
-    assert_response 204, "Did not successfully delete study file, expected response of 204 but found #{@response.response_code}"
+      execute_http_request(:patch,
+                           api_v1_study_study_share_path(study_id: @study.id, id: study_share_id),
+                           request_payload: update_attributes)
+      assert_response :success
+      assert json['deliver_emails'] == update_attributes[:study_share][:deliver_emails],
+             "Did not set deliver_emails correctly, expected " \
+                  "#{update_attributes[:study_share][:deliver_emails]} but found #{json['deliver_emails']}"
+      # delete study share
+      execute_http_request(:delete, api_v1_study_study_share_path(study_id: @study.id, id: study_share_id))
+      assert_response 204, "Did not successfully delete study file, expected response of 204 " \
+                           "but found #{@response.response_code}"
+    end
   end
 end
