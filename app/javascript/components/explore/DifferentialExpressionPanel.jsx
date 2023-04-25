6b86f6d19606a8406eb767e1e9d8c371c5d41958
Eric Weitz <eweitz@broadinstitute.org>
2023-04-25 15:43:09 -0400

    Port DE table headers to React Table v8

--- app/javascript/components/explore/DifferentialExpressionPanel.jsx
+++ app/javascript/components/explore/DifferentialExpressionPanel.jsx
@@ -148,6 +148,22 @@ function DifferentialExpressionTable({
   return (
     <>
       <table className="de-table table table-terra table-scp-compact">
+        <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => (
+                <th key={header.id}>
+                  {header.isPlaceholder ?
+                    null :
+                    flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
         <tbody>
           {table.getRowModel().rows.map(row => (
             <tr key={row.id}>
